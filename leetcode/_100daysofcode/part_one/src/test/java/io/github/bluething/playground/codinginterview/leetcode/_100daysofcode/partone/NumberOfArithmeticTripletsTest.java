18889ea407933fe7ae4d6173ab0788285a0a354e
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-19 22:39:17 +0700

    Number of Arithmetic Triplets 1st solution
    3 loops, need to refactor the algorithm

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/NumberOfArithmeticTripletsTest.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/NumberOfArithmeticTripletsTest.java
@@ -17,6 +17,16 @@ public class NumberOfArithmeticTripletsTest {
 
     private int arithmeticTriplets(int[] nums, int diff) {
         int result = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i+1; j < nums.length - 1; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    if ((nums[k] - nums[j] == diff) &&
+                            (nums[j] - nums[i] == diff)) {
+                        result++;
+                    }
+                }
+            }
+        }
 
         return result;
     }
