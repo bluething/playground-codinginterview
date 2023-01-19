8a142565ab6f9ba20bd4f39fc7856e1ed533f9ff
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-19 22:03:22 +0700

    Init

---
+++ leetcode/_100daysofcode/part_one/src/main/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/Main.java
@@ -0,0 +1,7 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
