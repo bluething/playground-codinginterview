f69c3cdb4680d1a75098ee4676eb6f9e60d1a4be
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-19 23:01:21 +0700

    Number of Arithmetic Triplets 3rd solution
    Using set to store visited value than we search if the set contain 2 values that satisfy the condition together with current value in the loop

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/NumberOfArithmeticTripletsTest.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/NumberOfArithmeticTripletsTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NumberOfArithmeticTripletsTest {
 
     @Test
@@ -23,6 +26,16 @@ public class NumberOfArithmeticTripletsTest {
     @Test
     public void case04() {
         Assertions.assertEquals(2, arithmeticTriplets2(new int[]{4,5,6,7,8,9}, 2));
+    }
+
+    @Test
+    public void case05() {
+        Assertions.assertEquals(2, arithmeticTriplets3(new int[]{0,1,4,6,7,10}, 3));
+    }
+
+    @Test
+    public void case06() {
+        Assertions.assertEquals(2, arithmeticTriplets3(new int[]{4,5,6,7,8,9}, 2));
     }
 
     private int arithmeticTriplets(int[] nums, int diff) {
@@ -59,6 +72,24 @@ public class NumberOfArithmeticTripletsTest {
                     j++;
                 }
             }
+        }
+
+        return result;
+    }
+
+    // from the problem description, 7 - 4 == 3 and 4 - 1 == 3
+    // what we want to do is find in the collection that satisfy this condition
+    // 1. Have value 7-3 = 4
+    // 2. Have value 7-(2*3) = 1
+    private int arithmeticTriplets3(int[] nums, int diff) {
+        int result = 0;
+        Set<Integer> visited = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if ((visited.contains(nums[i] - diff)) &&
+                    (visited.contains(nums[i] - diff * 2))) {
+                result++;
+            }
+            visited.add(nums[i]);
         }
 
         return result;
