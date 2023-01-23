944bb0eff26e1dcbf3b367bbc3186ad625614622
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-23 11:41:23 +0700

    Valid palindrom 1st solution
    Using two pointers. Move pinter until reach alphanumeric char then compare it as long as 1st pointer less than 2nd pointer.

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/ValidPalindromeTest.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/ValidPalindromeTest.java
@@ -26,6 +26,32 @@ public class ValidPalindromeTest {
     }
 
     private boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        s = s.toLowerCase();
+        while (i < j) {
+            while ((i < s.length() - 1) && !isAlphanumeric(s.charAt(i))) {
+                i++;
+            }
+            while ((j > 0) && !isAlphanumeric(s.charAt(j))) {
+                j--;
+            }
+
+            if ((i < j) && s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
         return true;
+    }
+
+    private boolean isAlphanumeric(char c) {
+        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57)) {
+            return true;
+        }
+
+        return false;
     }
 }
