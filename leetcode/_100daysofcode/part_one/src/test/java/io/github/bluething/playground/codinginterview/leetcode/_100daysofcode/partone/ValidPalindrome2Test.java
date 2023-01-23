c9cd195ffa60ede574a53017bbd4124ba86295ee
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-23 15:28:49 +0700

    Valid palindrome 2 2nd solution
    Using recursion

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/ValidPalindrome2Test.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/ValidPalindrome2Test.java
@@ -31,6 +31,31 @@ public class ValidPalindrome2Test {
         Assertions.assertFalse(validPalindrome("eeccccbebaeeabebccceea"));
     }
 
+    @Test
+    public void case06() {
+        Assertions.assertTrue(validPalindrome2("aba"));
+    }
+
+    @Test
+    public void case07() {
+        Assertions.assertTrue(validPalindrome2("abca"));
+    }
+
+    @Test
+    public void case08() {
+        Assertions.assertFalse(validPalindrome2("abc"));
+    }
+
+    @Test
+    public void case09() {
+        Assertions.assertTrue(validPalindrome2("deeee"));
+    }
+
+    @Test
+    public void case10() {
+        Assertions.assertFalse(validPalindrome2("eeccccbebaeeabebccceea"));
+    }
+
     private boolean validPalindrome(String s) {
         int i = 0;
         int j = s.length() - 1;
@@ -59,5 +84,30 @@ public class ValidPalindrome2Test {
         }
 
         return true;
+    }
+
+    private boolean validPalindrome2(String s) {
+        return isPalindromeRecs(s, 0, s.length() - 1, 1);
+    }
+
+    private boolean isPalindromeRecs(String s, int i, int j, int removeQuota) {
+        if (i >= j) {
+            return true;
+        }
+
+        // if the char match, recursively check is palindrome for the next char both left and right
+        if (s.charAt(i) == s.charAt(j)) {
+            return isPalindromeRecs(s, i + 1, j - 1, removeQuota);
+        }
+
+        // we must stop if we don't have quota to remove the char
+        if (removeQuota == 0) {
+            return false;
+        }
+
+        // if the char in left and right doesn't match
+        // check if the substring (eliminate either single char in left of right) is palindrome
+        // don't forget to reduce the quota
+        return isPalindromeRecs(s, i+1, j, removeQuota-1) || isPalindromeRecs(s, i, j-1, removeQuota-1);
     }
 }
