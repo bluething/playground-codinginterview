88ab6e1dd41100b1cf4c1ee598311f0282207da8
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-25 14:03:58 +0700

    3sum 2nd solution
    Using nested solution

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/ThreeSumTest.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/ThreeSumTest.java
@@ -27,6 +27,25 @@ public class ThreeSumTest {
         Assertions.assertEquals(expected, threeSum(new int[]{0,0,0}));
     }
 
+    @Test
+    public void case04() {
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(-1,-1,2),
+                Arrays.asList(-1,0,1));
+        Assertions.assertEquals(expected, threeSum2(new int[]{-1,0,1,2,-1,-4}));
+    }
+
+    @Test
+    public void case05() {
+        List<List<Integer>> expected = Collections.emptyList();
+        Assertions.assertEquals(expected, threeSum2(new int[]{0,1,1}));
+    }
+
+    @Test
+    public void case06() {
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(0,0,0));
+        Assertions.assertEquals(expected, threeSum2(new int[]{0,0,0}));
+    }
+
     private List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         int j, k, sum;
@@ -45,6 +64,41 @@ public class ThreeSumTest {
                     }
                     j++;
                     k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return triplets;
+    }
+
+    private List<List<Integer>> threeSum2(int[] nums) {
+        Arrays.sort(nums);
+        int j, k, sum;
+        List<List<Integer>> triplets = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            // avoid duplicate for i idx
+            if (i > 0 && (nums[i] == nums[i-1])) {
+                continue;
+            }
+            j = i+1;
+            k = nums.length - 1;
+            while (j < k) {
+                sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    triplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    // avoid duplicate for j idx
+                    while (j < k && (nums[j] == nums[j-1])) {
+                        j++;
+                    }
+                    // avoid duplicate for k idx
+                    while (j < k && (nums[k] == nums[k+1])) {
+                        k--;
+                    }
                 } else if (sum < 0) {
                     j++;
                 } else {
