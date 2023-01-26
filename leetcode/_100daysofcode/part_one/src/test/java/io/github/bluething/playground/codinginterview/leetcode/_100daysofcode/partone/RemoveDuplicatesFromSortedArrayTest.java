2eaca80068edec4fdaf97eda3bf98d72625ec6e2
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-26 14:53:35 +0700

    Remove Duplicates from Sorted Array 1st solution
    Using two pointers array

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/RemoveDuplicatesFromSortedArrayTest.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/RemoveDuplicatesFromSortedArrayTest.java
@@ -18,9 +18,19 @@ public class RemoveDuplicatesFromSortedArrayTest {
 
 
     private int removeDuplicates(int[] nums) {
-        int numOfUniqueElements = 0;
+        int numOfUniqueElements = 1;
+        int j = 1;
+        for (int i = 0; i < nums.length-1; i++) {
+            while (j < nums.length) {
+                if (nums[i] != nums[j]) {
+                    nums[i+1] = nums[j];
+                    numOfUniqueElements++;
+                    break;
+                }
+                j++;
+            }
+        }
 
-
-        return numOfUniqueElements
+        return numOfUniqueElements;
     }
 }
