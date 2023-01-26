7f3404793bd33e805f35450e483356651c673c6b
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-26 15:03:13 +0700

    Remove Duplicates from Sorted Array 2nd solution
    Use 2 pointer but without nested loop

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/RemoveDuplicatesFromSortedArrayTest.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/RemoveDuplicatesFromSortedArrayTest.java
@@ -16,6 +16,16 @@ public class RemoveDuplicatesFromSortedArrayTest {
         Assertions.assertEquals(5, removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
     }
 
+    @Test
+    public void case03() {
+        Assertions.assertEquals(2, removeDuplicates2(new int[]{1,1,2}));
+    }
+
+    @Test
+    public void case04() {
+        Assertions.assertEquals(5, removeDuplicates2(new int[]{0,0,1,1,1,2,2,3,3,4}));
+    }
+
 
     private int removeDuplicates(int[] nums) {
         int numOfUniqueElements = 1;
@@ -32,5 +42,23 @@ public class RemoveDuplicatesFromSortedArrayTest {
         }
 
         return numOfUniqueElements;
+    }
+
+    private int removeDuplicates2(int[] nums) {
+        // use two pointers, one pointing to unique element, the another one pointing to current element
+        int uniquePointer = 0;
+        for (int i = 1; i < nums.length; i++) {
+            // if we find unmatched element (means we find the next unique element
+            // increment the index that use as a pointer to unique element
+            // replace element that pointed by uniquePointer by value that pointed by i
+            if (nums[i] != nums[i-1]) {
+                uniquePointer++;
+                nums[uniquePointer] = nums[i];
+            }
+        }
+
+        // because we start the pointer from 0 (1st element)
+        // but actually the 1st element is the 1st unique element, so we need to add by 1 for correct answer
+        return uniquePointer+1;
     }
 }
