cbd219ba7ed494d6a5102cbdb39e9984f436b7f0
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-27 23:42:05 +0700

    Merge Sorted Array 1st solution

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/MergeSortedArrayTest.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/MergeSortedArrayTest.java
@@ -28,11 +28,37 @@ public class MergeSortedArrayTest {
         int[] nums1 = new int[]{0};
         int[] nums2 = new int[]{1};
         int[] expectedArray = new int[]{1};
-        merge(nums1, 1, nums2, 1);
+        merge(nums1, 0, nums2, 1);
+        Assertions.assertArrayEquals(expectedArray, nums1);
+    }
+
+    @Test
+    public void case04() {
+        int[] nums1 = new int[]{4,5,6,0,0,0};
+        int[] nums2 = new int[]{1,2,3};
+        int[] expectedArray = new int[]{1,2,3,4,5,6};
+        merge(nums1, 3, nums2, 3);
         Assertions.assertArrayEquals(expectedArray, nums1);
     }
 
     private void merge(int[] nums1, int m, int[] nums2, int n) {
+        // zero base index
+        int idx = m + n - 1;
+        m--;
+        n--;
+        while (m>=0 && n>=0) {
+            if (nums1[m] >= nums2[n]) {
+                nums1[idx--] = nums1[m--];
+            } else {
+                nums1[idx--] = nums2[n--];
+            }
+        }
 
+        // if there are remaining nums2
+        // the case is when nums1 have values greater than nums2 for all element
+        // or nums1 doesn't fill with value (m==0)
+        while (n >= 0) {
+            nums1[idx--] = nums2[n--];
+        }
     }
 }
