90917e1c9b4bcf90f518fac04f0734b230a99a84
Habib Ali Machpud <habib.machpud@gmail.com>
2023-01-30 15:11:25 +0700

    Majority Element 3rd solution
    Moore's majority vote algorithm

--- leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/MajorityElementTest.java
+++ leetcode/_100daysofcode/part_one/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partone/MajorityElementTest.java
@@ -29,6 +29,16 @@ public class MajorityElementTest {
         Assertions.assertEquals(2, majorityElement2(new int[]{2,2,1,1,1,2,2}));
     }
 
+    @Test
+    public void case05() {
+        Assertions.assertEquals(3, majorityElement3(new int[]{3,2,3}));
+    }
+
+    @Test
+    public void case06() {
+        Assertions.assertEquals(2, majorityElement3(new int[]{2,2,1,1,1,2,2}));
+    }
+
     private int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length/2];
@@ -49,5 +59,28 @@ public class MajorityElementTest {
             }
         }
         return result;
+    }
+
+    // Boyer–Moore majority vote algorithm
+    // This algorithm works on the fact that if an element occurs more than N/2 times,
+    // it means that the remaining elements other than this would definitely be less than N/2.
+    private int majorityElement3(int[] nums) {
+        int counter = 0;
+        int major = 0;
+        for (int num : nums) {
+            // the major candidate
+            if (counter == 0) {
+                major = num;
+            }
+            // increment it when see the same value
+            // otherwise decrease it to down vote the majority and will be replaced when the counter reach 0
+            if (num == major) {
+                counter++;
+            } else {
+                counter--;
+            }
+        }
+
+        return major;
     }
 }
