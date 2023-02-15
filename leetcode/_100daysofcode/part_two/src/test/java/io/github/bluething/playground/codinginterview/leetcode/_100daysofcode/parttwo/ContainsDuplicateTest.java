dfdb1898d55fbaf19d303591d9b41dd316d6f8d5
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-15 16:05:11 +0700

    Contains Duplicate problem

---
+++ leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/ContainsDuplicateTest.java
@@ -0,0 +1,30 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.parttwo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/contains-duplicate/
+class ContainsDuplicateTest {
+
+    @Test
+    void case01() {
+        Assertions.assertTrue(containsDuplicate(new int[]{1,2,3,1}));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertFalse(containsDuplicate(new int[]{1,2,3,4}));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertTrue(containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
+    }
+
+    private boolean containsDuplicate(int[] nums) {
+        boolean isDuplicate = false;
+
+
+        return isDuplicate;
+    }
+}
