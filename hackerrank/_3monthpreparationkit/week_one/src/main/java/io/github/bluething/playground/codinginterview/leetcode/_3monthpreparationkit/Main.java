e0f6df558bf0033fef98bec37dbb456045271201
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-15 15:14:17 +0700

    Add new module for new iteration

---
+++ hackerrank/_3monthpreparationkit/week_one/src/main/java/io/github/bluething/playground/codinginterview/leetcode/_3monthpreparationkit/Main.java
@@ -0,0 +1,7 @@
+package io.github.bluething.playground.codinginterview.leetcode._3monthpreparationkit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
