94fb1b73845a972784dadebf8ee1de257037b708
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-15 17:12:31 +0700

    Number of Arithmetic Triplets 2nd solution

--- leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/NumberOfArithmeticTripletsTest.java
+++ leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/NumberOfArithmeticTripletsTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class NumberOfArithmeticTripletsTest {
 
     @Test
@@ -13,6 +16,16 @@ class NumberOfArithmeticTripletsTest {
     @Test
     void case02() {
         Assertions.assertEquals(2, arithmeticTriplets(new int[]{4,5,6,7,8,9}, 2));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertEquals(2, arithmeticTriplets2(new int[]{0,1,4,6,7,10}, 3));
+    }
+
+    @Test
+    void case04() {
+        Assertions.assertEquals(2, arithmeticTriplets2(new int[]{4,5,6,7,8,9}, 2));
     }
 
     private int arithmeticTriplets(int[] nums, int diff) {
@@ -36,6 +49,27 @@ class NumberOfArithmeticTripletsTest {
                     j++;
                 }
             }
+        }
+
+        return result;
+    }
+
+    // from problem description we can see that
+    // nums[j] - nums[i] == diff
+    // nums[k] - nums[j] == diff
+    // ------------------------- +
+    // nums[k] - nums[i] = 2*diff => nums[i] - 2*diff = nums[k]
+    // so we only need to care about nums[i] -> single loop
+    private int arithmeticTriplets2(int[] nums, int diff) {
+        int result = 0;
+        Set<Integer> visitedValues = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            // check if the set contain values from above formula
+            if (visitedValues.contains(nums[i] - diff) &&
+            visitedValues.contains(nums[i] - 2 * diff)) {
+                result++;
+            }
+            visitedValues.add(nums[i]);
         }
 
         return result;
