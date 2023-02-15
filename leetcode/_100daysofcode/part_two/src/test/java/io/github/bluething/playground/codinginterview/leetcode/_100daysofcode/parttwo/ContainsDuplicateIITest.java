0fb2300ff38e6a81a63604c6ed9481645196c608
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-15 16:41:46 +0700

    Contains Duplicate II 2nd solution

--- leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/ContainsDuplicateIITest.java
+++ leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/ContainsDuplicateIITest.java
@@ -3,7 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 // https://leetcode.com/problems/contains-duplicate-ii/description/
@@ -24,6 +26,26 @@ class ContainsDuplicateIITest {
         Assertions.assertFalse(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertTrue(containsNearbyDuplicate2(new int[]{1,2,3,1}, 3));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertTrue(containsNearbyDuplicate2(new int[]{1,0,1,1}, 1));
+    }
+
+    @Test
+    void case06() {
+        Assertions.assertFalse(containsNearbyDuplicate2(new int[]{1,2,3,1,2,3}, 2));
+    }
+
+    @Test
+    void case07() {
+        Assertions.assertTrue(containsNearbyDuplicate2(new int[]{99,99}, 2));
+    }
+
     private boolean containsNearbyDuplicate(int[] nums, int k) {
         if (k == 0) {
             return false;
@@ -39,6 +61,22 @@ class ContainsDuplicateIITest {
                 slidingWindow.remove(nums[i-k]);
             }
             slidingWindow.add(nums[i]);
+        }
+
+        return false;
+    }
+
+    private boolean containsNearbyDuplicate2(int[] nums, int k) {
+        if (k == 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> valIdx = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (valIdx.containsKey(nums[i]) && i - valIdx.get(nums[i]) <= k) {
+                return true;
+            }
+            valIdx.put(nums[i], i);
         }
 
         return false;
