cf7f09cbab584416fd8fa7dd646612583146c69d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-17 23:13:53 +0700

    Valid anagram solution

--- leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/ValidAnagramTest.java
+++ leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/ValidAnagramTest.java
@@ -14,7 +14,29 @@ class ValidAnagramTest {
         Assertions.assertFalse(isAnagram("rat", "car"));
     }
 
+    @Test
+    void case03() {
+        Assertions.assertFalse(isAnagram("zlap", "kcqx"));
+    }
+
+    @Test
+    void case04() {
+        Assertions.assertFalse(isAnagram("ab", "b"));
+    }
+
     private boolean isAnagram(String s, String t) {
+        int[] alphabetIdx = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            alphabetIdx[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            alphabetIdx[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < alphabetIdx.length; i++) {
+            if (alphabetIdx[i] != 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
