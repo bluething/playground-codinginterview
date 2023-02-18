d4af9fe2a0238f0cb3315719d2253d9b504b942b
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-18 23:39:49 +0700

    Valid Parentheses 1st solution

--- leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/ValidParenthesesTest.java
+++ leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/ValidParenthesesTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 public class ValidParenthesesTest {
     @Test
     void case01() {
@@ -19,7 +21,41 @@ public class ValidParenthesesTest {
         Assertions.assertFalse(isValid("(]"));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertFalse(isValid("]"));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertFalse(isValid("["));
+    }
+
     private boolean isValid(String s) {
-        return false;
+        Stack<Character> openParnetheses = new Stack<>();
+        char open;
+        char close;
+
+        for (int i = 0; i < s.length(); i++) {
+            open = s.charAt(i);
+            if (open == '(' || open == '[' || open == '{') {
+                openParnetheses.push(open);
+            } else {
+                if (openParnetheses.isEmpty()) {
+                    return false;
+                }
+                open  = openParnetheses.pop();
+                close = s.charAt(i);
+                if ((open == '(' && close != ')') || (open == '[' && close != ']') || (open == '{' && close != '}')) {
+                    return false;
+                }
+            }
+        }
+
+        if (!openParnetheses.isEmpty()) {
+            return false;
+        }
+
+        return true;
     }
 }
