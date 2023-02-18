ed79b0f0c3f43aeff20a91aa91bd3a90f0af481e
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-18 23:58:19 +0700

    Best Time to Buy and Sell Stock problem

---
+++ leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,25 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.parttwo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+public class BestTimeToBuyAndSellStockTest {
+
+    @Test
+    void case01() {
+        Assertions.assertEquals(5, maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertEquals(0, maxProfit(new int[]{7,6,4,3,1}));
+    }
+
+    private int maxProfit(int[] prices) {
+        int provit = 0;
+        
+
+        return provit;
+    }
+}
