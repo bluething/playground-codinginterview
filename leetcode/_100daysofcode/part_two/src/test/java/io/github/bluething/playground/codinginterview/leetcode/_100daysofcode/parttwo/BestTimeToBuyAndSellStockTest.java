a8a403db45e2a670a0cbb79e8960351360ca770e
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-19 00:06:28 +0700

    Best Time to Buy and Sell Stock 2nd solution

--- leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/BestTimeToBuyAndSellStockTest.java
+++ leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/BestTimeToBuyAndSellStockTest.java
@@ -16,6 +16,16 @@ public class BestTimeToBuyAndSellStockTest {
         Assertions.assertEquals(0, maxProfit(new int[]{7,6,4,3,1}));
     }
 
+    @Test
+    void case03() {
+        Assertions.assertEquals(5, maxProfit2(new int[]{7,1,5,3,6,4}));
+    }
+
+    @Test
+    void case04() {
+        Assertions.assertEquals(0, maxProfit2(new int[]{7,6,4,3,1}));
+    }
+
     private int maxProfit(int[] prices) {
         int provit = 0;
         int buy = prices[0];
@@ -25,6 +35,17 @@ public class BestTimeToBuyAndSellStockTest {
             } else {
                 provit = Math.max(provit, prices[i] - buy);
             }
+        }
+
+        return provit;
+    }
+
+    private int maxProfit2(int[] prices) {
+        int provit = 0;
+        int buy = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            provit = Math.max(provit, prices[i] - buy);
+            buy = Math.min(buy, prices[i]);
         }
 
         return provit;
