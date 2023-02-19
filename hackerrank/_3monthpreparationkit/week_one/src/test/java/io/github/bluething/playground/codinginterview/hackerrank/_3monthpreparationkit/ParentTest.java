17bec085984cd2742de982393bdc8867bb3edbcb
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-19 15:06:20 +0700

    Fix parent test
    The stream not cleared after each test

--- hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/ParentTest.java
+++ hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/ParentTest.java
@@ -1,22 +1,22 @@
 package io.github.bluething.playground.codinginterview.hackerrank._3monthpreparationkit;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public abstract class ParentTest {
-    public static final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    public static final PrintStream originalOutput = System.out;
+    public final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    public final PrintStream originalOutput = System.out;
 
-    @BeforeAll
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         System.setOut(new PrintStream(output));
     }
 
-    @AfterAll
-    public static void tearDown() {
+    @AfterEach
+    public void tearDown() {
         System.setOut(originalOutput);
     }
 }
