8813856b1ba1579bc345c931c9f4287f5deb71f2
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-19 15:06:52 +0700

    Min max sum 1st solution

--- hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/MinMaxSumTest.java
+++ hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/MinMaxSumTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 // https://www.hackerrank.com/challenges/three-month-preparation-kit-mini-max-sum/problem
@@ -16,12 +17,20 @@ public class MinMaxSumTest extends ParentTest {
         Assertions.assertEquals("16 24", output.toString());
     }
 
+    @Test
     void case02() {
         miniMaxSum(Arrays.asList(1,2,3,4,5));
         Assertions.assertEquals("10 14", output.toString());
     }
     private void miniMaxSum(List<Integer> arr) {
-        // Write your code here
+        arr.sort(Comparator.naturalOrder());
+        int min = 0;
+        long max = 0L;
+        for (int i = 0; i < arr.size()-1; i++) {
+            min += arr.get(i);
+            max += arr.get(i+1);
+        }
 
+        System.out.print(min + " " + max);
     }
 }
