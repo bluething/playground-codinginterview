908f04ed1255aeca3dcd64fa2b48a0cadb459457
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-19 16:51:59 +0700

    Divisible Sum Pairs 1st solution

--- hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/DivisibleSumPairsTest.java
+++ hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/DivisibleSumPairsTest.java
@@ -20,6 +20,14 @@ public class DivisibleSumPairsTest {
     }
 
     private int divisibleSumPairs(int n, int k, List<Integer> ar) {
-        return 0;
+        int numOfPair = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if ((ar.get(i) + ar.get(j)) % k == 0) {
+                    numOfPair++;
+                }
+            }
+        }
+        return numOfPair;
     }
 }
