f1d3f977a30c242b1d75028b409490c4002f4aff
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-19 15:32:16 +0700

    Breaking the Records problem

---
+++ hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/BreakingTheRecordTest.java
@@ -0,0 +1,19 @@
+package io.github.bluething.playground.codinginterview.hackerrank._3monthpreparationkit.weekone;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-breaking-best-and-worst-records/problem
+public class BreakingTheRecordTest {
+
+    void case01() {
+        Assertions.assertEquals(Arrays.asList(1, 1), breakingRecords(Arrays.asList(12, 24, 10, 24)));
+    }
+
+    private List<Integer> breakingRecords(List<Integer> scores) {
+        return Collections.emptyList();
+    }
+}
