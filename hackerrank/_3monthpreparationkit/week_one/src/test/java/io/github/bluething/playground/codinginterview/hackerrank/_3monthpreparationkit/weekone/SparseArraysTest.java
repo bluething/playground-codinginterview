469783662b3b5d4d4e9a34153eacba5cf90b859f
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-19 18:47:09 +0700

    Sparse Arrays 2nd solution

--- hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/SparseArraysTest.java
+++ hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/SparseArraysTest.java
@@ -26,6 +26,25 @@ public class SparseArraysTest {
                         Arrays.asList("abcde", "sdaklfj", "asdjf", "na", "basdn")));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertEquals(Arrays.asList(2, 1, 0),
+                matchingStrings2(Arrays.asList("aba", "baba", "aba", "xzxb"), Arrays.asList("aba", "xzxb", "ab")));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertEquals(Arrays.asList(1, 0, 1),
+                matchingStrings2(Arrays.asList("def", "de", "fgh"), Arrays.asList("de", "lmn", "fgh")));
+    }
+
+    @Test
+    void case06() {
+        Assertions.assertEquals(Arrays.asList(1, 3, 4, 3, 2),
+                matchingStrings2(Arrays.asList("abcde", "sdaklfj", "asdjf", "na", "basdn", "sdaklfj", "asdjf", "na", "asdjf", "na", "basdn", "sdaklfj", "asdjf"),
+                        Arrays.asList("abcde", "sdaklfj", "asdjf", "na", "basdn")));
+    }
+
     private List<Integer> matchingStrings(List<String> strings, List<String> queries) {
         Map<String, Integer> queryCounters = new HashMap<>();
         for (String query : queries) {
@@ -43,5 +62,18 @@ public class SparseArraysTest {
         }
 
         return results;
+    }
+
+    private List<Integer> matchingStrings2(List<String> strings, List<String> queries) {
+        Map<String, Integer> stringCounter = new HashMap<>();
+        for (String string : strings) {
+            stringCounter.put(string, stringCounter.getOrDefault(string, 0) + 1);
+        }
+        List<Integer> results = new ArrayList<>();
+        for (String query : queries) {
+            results.add(stringCounter.getOrDefault(query, 0));
+        }
+        return results;
+
     }
 }
