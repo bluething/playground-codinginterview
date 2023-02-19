062c350d2243dc112c31aba10f9a430f6a574666
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-19 16:47:07 +0700

    Camel Case 4 solution

--- hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/CamelCase4Test.java
+++ hackerrank/_3monthpreparationkit/week_one/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekone/CamelCase4Test.java
@@ -43,6 +43,61 @@ public class CamelCase4Test extends ParentTest {
     }
 
     private void convertToCamelCase(String input) {
-        
+        String[] inputs = input.split("\\;+");
+        Operator operator;
+        if (inputs[0].equals("S")) {
+            operator = new Split();
+        } else {
+            operator = new Combine();
+        }
+
+        System.out.print(operator.operate(inputs[1], inputs[2]));
+    }
+}
+
+
+
+interface Operator {
+    String operate(String type, String source);
+}
+
+class Split implements Operator {
+
+    @Override
+    public String operate(String type, String source) {
+        if (type.equals("M")) {
+            source = source.substring(0, source.indexOf('('));
+        }
+
+        StringBuilder splitted = new StringBuilder();
+        String[] sources = source.split("(?=\\p{Upper})");
+        for (int i = 0; i < sources.length; i++) {
+            splitted.append(sources[i].toLowerCase()).append(" ");
+        }
+
+        return splitted.toString().trim();
+    }
+}
+class Combine implements Operator {
+    @Override
+    public String operate(String type, String source) {
+        String[] value = source.split("\\s+");
+        StringBuilder combined = new StringBuilder();
+
+        if (type.equals("C")) {
+            combined.append(value[0].substring(0,1).toUpperCase()).append(value[0].substring(1));
+        } else {
+            combined.append(value[0]);
+        }
+
+        for (int i = 1; i < value.length; i++) {
+            combined.append(value[i].substring(0,1).toUpperCase()).append(value[i].substring(1));
+        }
+
+        if (type.equals("M")) {
+            combined.append("()");
+        }
+
+        return combined.toString();
     }
 }
