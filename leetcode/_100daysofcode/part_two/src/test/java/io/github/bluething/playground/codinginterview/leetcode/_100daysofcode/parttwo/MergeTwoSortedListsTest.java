f648be984b9617116ff5b1bc5d7605f510d3cfb8
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-20 22:54:13 +0700

    Add test case

--- leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/MergeTwoSortedListsTest.java
+++ leetcode/_100daysofcode/part_two/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/parttwo/MergeTwoSortedListsTest.java
@@ -1,25 +1,41 @@
 package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.parttwo;
 
+import org.junit.jupiter.api.Test;
+
 // https://leetcode.com/problems/merge-two-sorted-lists/description/
 public class MergeTwoSortedListsTest {
+
+    @Test
+    public void case01() {
+        ListNode fourOne = new ListNode(4);
+        ListNode twoOne = new ListNode(2, fourOne);
+        ListNode oneOne = new ListNode(1, twoOne);
+
+        ListNode fourTwo = new ListNode(4);
+        ListNode threeTwo = new ListNode(3, fourTwo);
+        ListNode oneTwo = new ListNode(1, threeTwo);
+
+        mergeTwoLists(oneTwo, oneOne);
+    }
+
     private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         return null;
     }
-}
 
-class ListNode {
-    int val;
-    ListNode next;
+    class ListNode {
+        int val;
+        io.github.bluething.playground.codinginterview.leetcode._100daysofcode.parttwo.ListNode next;
 
-    ListNode() {
-    }
+        ListNode() {
+        }
 
-    ListNode(int val) {
-        this.val = val;
-    }
+        ListNode(int val) {
+            this.val = val;
+        }
 
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+        ListNode(int val, io.github.bluething.playground.codinginterview.leetcode._100daysofcode.parttwo.ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
