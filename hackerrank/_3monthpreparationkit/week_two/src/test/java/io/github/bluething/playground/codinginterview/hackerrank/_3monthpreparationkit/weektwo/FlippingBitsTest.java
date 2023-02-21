aa6433bcf64a29efc4bf3ff8b8692d3118748e7d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-21 23:44:44 +0700

    Flipping bits problem

---
+++ hackerrank/_3monthpreparationkit/week_two/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weektwo/FlippingBitsTest.java
@@ -0,0 +1,26 @@
+package io.github.bluething.playground.codinginterview.hackerrank._3monthpreparationkit.weektwo;
+
+import io.github.bluething.playground.codinginterview.hackerrank._3monthpreparationkit.ParentTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FlippingBitsTest extends ParentTest {
+    @Test
+    public void case01() {
+        Assertions.assertEquals(2147483648L, flippingBits(2147483647L));
+    }
+
+    @Test
+    public void case02() {
+        Assertions.assertEquals(4294967294L, flippingBits(1L));
+    }
+
+    @Test
+    public void case03() {
+        Assertions.assertEquals(4294967295L, flippingBits(0L));
+    }
+
+    private long flippingBits(long n) {
+        return 0L;
+    }
+}
