1969b88b64a36228421bfd4c61422c0965509d17
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-21 23:17:24 +0700

    Lonely Integer 2nd solution

--- hackerrank/_3monthpreparationkit/week_two/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weektwo/LonelyIntegerTest.java
+++ hackerrank/_3monthpreparationkit/week_two/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weektwo/LonelyIntegerTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 // https://www.hackerrank.com/challenges/three-month-preparation-kit-lonely-integer/problem
 public class LonelyIntegerTest extends ParentTest {
@@ -17,7 +19,7 @@ public class LonelyIntegerTest extends ParentTest {
 
     @Test
     void case02() {
-        Assertions.assertEquals(4, lonelyinteger(Arrays.asList(1, 2, 3, 4, 3, 2, 1)));
+        Assertions.assertEquals(4, lonelyinteger2(Arrays.asList(1, 2, 3, 4, 3, 2, 1)));
     }
 
     // With XOR operator:
@@ -30,5 +32,24 @@ public class LonelyIntegerTest extends ParentTest {
             lonely = lonely ^ val;
         }
         return lonely;
+    }
+
+    private int lonelyinteger2(List<Integer> a) {
+        int[] marks = new int[101];
+        for (int val : a) {
+            if (marks[val] < 0) {
+                marks[val]++;
+            } else {
+                marks[val]--;
+            }
+        }
+
+        for (int i = 0; i < marks.length; i++) {
+            if (marks[i] < 0) {
+                return i;
+            }
+        }
+
+        return 0;
     }
 }
