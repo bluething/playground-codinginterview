302afb6c31498fc8f493fa1665b008208a76b293
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-21 22:57:13 +0700

    Week two hackerrank module

---
+++ hackerrank/_3monthpreparationkit/week_two/src/main/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/Main.java
@@ -0,0 +1,7 @@
+package io.github.bluething.playground.codinginterview.hackerrank._3monthpreparationkit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
