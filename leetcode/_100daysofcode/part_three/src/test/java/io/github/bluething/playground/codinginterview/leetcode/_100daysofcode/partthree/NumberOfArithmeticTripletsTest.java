be4a3fc76f1c3fd527d2c9c6966cf9c1870ca4bf
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-27 23:09:18 +0700

    Number of Arithmetic Triplets 3rd solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/NumberOfArithmeticTripletsTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/NumberOfArithmeticTripletsTest.java
@@ -3,6 +3,7 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.Set;
 
 // https://leetcode.com/problems/number-of-arithmetic-triplets/
@@ -25,6 +26,16 @@ public class NumberOfArithmeticTripletsTest {
     @Test
     public void case04() {
         Assertions.assertEquals(2, arithmeticTriplets2(new int[]{4,5,6,7,8,9}, 2));
+    }
+
+    @Test
+    public void case05() {
+        Assertions.assertEquals(2, arithmeticTriplets3(new int[]{0,1,4,6,7,10}, 3));
+    }
+
+    @Test
+    public void case06() {
+        Assertions.assertEquals(2, arithmeticTriplets3(new int[]{4,5,6,7,8,9}, 2));
     }
 
     private int arithmeticTriplets(int[] nums, int diff) {
@@ -60,6 +71,23 @@ public class NumberOfArithmeticTripletsTest {
                     j++;
                 }
             }
+        }
+
+        return result;
+    }
+
+    // nums[j] - nums[i] == diff
+    // nums[k] - nums[j] == diff
+    // for each iteration we only know 1 of 3 values, its nums[i]
+    // so we need to check if our collections contain the rest
+    private int arithmeticTriplets3(int[] nums, int diff) {
+        int result = 0;
+        Set<Integer> visitedNum = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (visitedNum.contains(nums[i]-diff) && visitedNum.contains(nums[i] - 2*diff)) {
+                result++;
+            }
+            visitedNum.add(nums[i]);
         }
 
         return result;
