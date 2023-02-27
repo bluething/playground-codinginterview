c8eb34cd14b091b024cfe12c3b6a1ca3110fe144
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-27 22:54:37 +0700

    Number of Arithmetic Triplets 1st solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/NumberOfArithmeticTripletsTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/NumberOfArithmeticTripletsTest.java
@@ -17,6 +17,16 @@ public class NumberOfArithmeticTripletsTest {
         Assertions.assertEquals(2, arithmeticTriplets(new int[]{4,5,6,7,8,9}, 2));
     }
 
+    @Test
+    public void case03() {
+        Assertions.assertEquals(2, arithmeticTriplets2(new int[]{0,1,4,6,7,10}, 3));
+    }
+
+    @Test
+    public void case04() {
+        Assertions.assertEquals(2, arithmeticTriplets2(new int[]{4,5,6,7,8,9}, 2));
+    }
+
     private int arithmeticTriplets(int[] nums, int diff) {
         int result = 0;
         for (int i = 0; i < nums.length-2; i++) {
@@ -26,6 +36,28 @@ public class NumberOfArithmeticTripletsTest {
                     && nums[k]-nums[j] == diff) {
                         result++;
                     }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int arithmeticTriplets2(int[] nums, int diff) {
+        int result = 0;
+        for (int i = 0; i < nums.length-2; i++) {
+            int j = i+1;
+            int k = j+1;
+            while (k < nums.length) {
+                if (nums[j]-nums[i] == diff
+                        && nums[k]-nums[j] == diff) {
+                    result++;
+                    j++;
+                    k--;
+                } else if (nums[k]-nums[j] < diff) {
+                    k++;
+                } else {
+                    j++;
                 }
             }
         }
