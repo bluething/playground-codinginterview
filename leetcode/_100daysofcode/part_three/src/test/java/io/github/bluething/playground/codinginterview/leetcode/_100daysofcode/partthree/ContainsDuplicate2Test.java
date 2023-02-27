89599135d003fa6e66796dc06c47e1989b4a0d94
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-27 22:40:44 +0700

    Contain duplicate 2 2nd solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ContainsDuplicate2Test.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ContainsDuplicate2Test.java
@@ -3,7 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 // https://leetcode.com/problems/contains-duplicate-ii/
@@ -23,7 +25,36 @@ public class ContainsDuplicate2Test {
         Assertions.assertFalse(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2));
     }
 
+    @Test
+    public void case04() {
+        Assertions.assertTrue(containsNearbyDuplicate2(new int[]{1,2,3,1}, 3));
+    }
+
+    @Test
+    public void case05() {
+        Assertions.assertTrue(containsNearbyDuplicate2(new int[]{1,0,1,1}, 1));
+    }
+
+    @Test
+    public void case06() {
+        Assertions.assertFalse(containsNearbyDuplicate2(new int[]{1,2,3,1,2,3}, 2));
+    }
+
+    @Test
+    public void case07() {
+        Assertions.assertFalse(containsNearbyDuplicate2(new int[]{1,2,1}, 0));
+    }
+
+    @Test
+    public void case08() {
+        Assertions.assertFalse(containsNearbyDuplicate(new int[]{1,2,1}, 0));
+    }
+
     private boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (k == 0) {
+            return false;
+        }
+
         Set<Integer> slidingWindows = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             if (!slidingWindows.add(nums[i])) {
@@ -36,6 +67,24 @@ public class ContainsDuplicate2Test {
             }
             slidingWindows.add(nums[i]);
 
+        }
+        return false;
+    }
+
+    private boolean containsNearbyDuplicate2(int[] nums, int k) {
+        if (k == 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> numsCounter = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numsCounter.get(nums[i]) != null) {
+                return true;
+            }
+            if (i >= k) {
+                numsCounter.remove(nums[i-k]);
+            }
+            numsCounter.put(nums[i], i);
         }
         return false;
     }
