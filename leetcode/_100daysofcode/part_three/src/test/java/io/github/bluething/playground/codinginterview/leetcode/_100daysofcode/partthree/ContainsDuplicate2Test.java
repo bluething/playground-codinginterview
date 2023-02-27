3ae7d7e95fd39843db195825ac4008d5b7d6ab8d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-02-27 21:58:07 +0700

    Contain duplicate 2 problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ContainsDuplicate2Test.java
@@ -0,0 +1,27 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/contains-duplicate-ii/
+public class ContainsDuplicate2Test {
+    @Test
+    void case01() {
+        Assertions.assertTrue(containsNearbyDuplicate(new int[]{1,2,3,1}, 3));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertTrue(containsNearbyDuplicate(new int[]{1,0,1,1}, 1));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertFalse(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2));
+    }
+
+    private boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        return false;
+    }
+}
