a94cd7bde6080cad10a8826972b6786d7c442284
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-01 22:29:13 +0700

    Valid anagram problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ValidAnagramTest.java
@@ -0,0 +1,27 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/valid-anagram/
+public class ValidAnagramTest {
+    @Test
+    void case01() {
+        Assertions.assertTrue(isAnagram("anagram", "nagaram"));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertFalse(isAnagram("rat", "car"));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertFalse(isAnagram("a", "ab"));
+    }
+
+    private boolean isAnagram(String s, String t) {
+
+        return true;
+    }
+}
