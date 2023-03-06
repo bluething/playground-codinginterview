9c8fc2c6745088c444b676c9adde6191bd307959
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-06 22:24:50 +0700

    Valid Palindrome 2 solution 2
    Using recursion

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ValidPalindromeIITest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ValidPalindromeIITest.java
@@ -29,6 +29,32 @@ public class ValidPalindromeIITest {
     public void case05() {
         Assertions.assertFalse(validPalindrome("eeccccbebaeeabebccceea"));
     }
+
+    @Test
+    public void case06() {
+        Assertions.assertTrue(validPalindrome2("aba"));
+    }
+
+    @Test
+    public void case07() {
+        Assertions.assertTrue(validPalindrome2("abca"));
+    }
+
+    @Test
+    public void case08() {
+        Assertions.assertFalse(validPalindrome2("abc"));
+    }
+
+    @Test
+    public void case09() {
+        Assertions.assertTrue(validPalindrome2("deeee"));
+    }
+
+    @Test
+    public void case10() {
+        Assertions.assertFalse(validPalindrome2("eeccccbebaeeabebccceea"));
+    }
+
     private boolean validPalindrome(String s) {
         int i = 0;
         int j = s.length()-1;
@@ -55,5 +81,26 @@ public class ValidPalindromeIITest {
             j--;
         }
         return true;
+    }
+
+    private boolean validPalindrome2(String s) {
+        return isPalindromeRec(s, 0, s.length()-1, 1);
+    }
+
+    private boolean isPalindromeRec (String s, int i, int j, int removeQuota) {
+        if (i >= j) {
+            return true;
+        }
+        // continue checking palindrome without reduce the quota
+        if (s.charAt(i) == s.charAt(j)) {
+            return isPalindromeRec(s, i+1, j-1, removeQuota);
+        }
+        // stop if we don't have any quota for deletion
+        if (removeQuota == 0) {
+            return false;
+        }
+        // continue checking palindrome with reduce the quota
+        // must check the char after/before unequal char
+        return isPalindromeRec(s, i+1, j, removeQuota-1) || isPalindromeRec(s, i, j-1, removeQuota-1);
     }
 }
