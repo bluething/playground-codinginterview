b945fb93c11044921f0d059fed928fd3c9135f83
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-07 14:56:16 +0700

    Find First Palindromic String in the Array solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/FindFirstPalindromicStringInTheArrayTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/FindFirstPalindromicStringInTheArrayTest.java
@@ -21,7 +21,24 @@ public class FindFirstPalindromicStringInTheArrayTest {
     }
 
     private String firstPalindrome(String[] words) {
-
+        for (int i = 0; i < words.length; i++) {
+            if (isPalindrome(words[i])) {
+                return words[i];
+            }
+        }
         return "";
+    }
+
+    private boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
