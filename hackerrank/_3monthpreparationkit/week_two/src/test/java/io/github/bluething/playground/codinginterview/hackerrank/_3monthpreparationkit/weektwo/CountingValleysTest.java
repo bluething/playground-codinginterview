a955e136640835544ebb3dcf5ad6a23d16319ec7
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-07 16:53:31 +0700

    Counting Valleys problem

---
+++ hackerrank/_3monthpreparationkit/week_two/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weektwo/CountingValleysTest.java
@@ -0,0 +1,16 @@
+package io.github.bluething.playground.codinginterview.hackerrank._3monthpreparationkit.weektwo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-counting-valleys/problem
+public class CountingValleysTest {
+    @Test
+    public void case01() {
+        Assertions.assertEquals(1, countingValleys(8, "UDDDUDUU"));
+    }
+
+    private int countingValleys(int steps, String path) {
+        return 0;
+    }
+}
