26313fffe49ace40c2506978f20e7c5514671168
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-08 14:49:59 +0700

    Mars Exploration problem

---
+++ hackerrank/_3monthpreparationkit/week_two/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weektwo/MarsExplorationTest.java
@@ -0,0 +1,20 @@
+package io.github.bluething.playground.codinginterview.hackerrank._3monthpreparationkit.weektwo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MarsExplorationTest {
+    @Test
+    public void case01() {
+        Assertions.assertEquals(3, marsExploration("SOSSPSSQSSOR"));
+    }
+
+    @Test
+    public void case02() {
+        Assertions.assertEquals(1, marsExploration("SOSSOT"));
+    }
+
+    private int marsExploration(String s) {
+        return 0;
+    }
+}
