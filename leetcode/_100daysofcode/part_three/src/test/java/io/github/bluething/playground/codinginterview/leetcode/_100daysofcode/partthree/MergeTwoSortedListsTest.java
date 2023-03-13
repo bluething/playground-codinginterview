5e8cb21f063c81206a66211bebdb70809a8014b0
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-13 14:29:50 +0700

    Merge Two Sorted Lists solution suing recursive

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MergeTwoSortedListsTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MergeTwoSortedListsTest.java
@@ -2,9 +2,27 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 
 public class MergeTwoSortedListsTest {
 
+    // compare the head
+    // for node that have smaller value, give the head to the merged list
+    // recursively, find for the next merged node
     private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
-        return null;
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode merged = new ListNode();
+        if (list1.val <= list2.val) {
+            merged = list1;
+            merged.next = mergeTwoLists(list1.next, list2);
+        }
+        else {
+            merged = list2;
+            merged.next = mergeTwoLists(list1, list2.next);
+        }
+        return merged;
     }
 
     class ListNode {
