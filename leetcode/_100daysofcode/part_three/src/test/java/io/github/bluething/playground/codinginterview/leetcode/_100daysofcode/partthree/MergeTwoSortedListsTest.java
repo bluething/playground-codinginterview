15afc70262dbafefd59ae71a8198966afb577bc7
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-13 14:35:12 +0700

    Merge Two Sorted Lists 2nd solution using pointer helper

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MergeTwoSortedListsTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MergeTwoSortedListsTest.java
@@ -1,6 +1,34 @@
 package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
 
+import org.junit.jupiter.api.Test;
+
 public class MergeTwoSortedListsTest {
+
+    @Test
+    void case01() {
+        ListNode fourOne = new ListNode(4);
+        ListNode twoOne = new ListNode(2, fourOne);
+        ListNode oneOne = new ListNode(1, twoOne);
+
+        ListNode fourTwo = new ListNode(4);
+        ListNode threeTwo = new ListNode(3, fourTwo);
+        ListNode oneTwo = new ListNode(1, threeTwo);
+
+        mergeTwoLists(oneTwo, oneOne);
+    }
+
+    @Test
+    void case02() {
+        ListNode fourOne = new ListNode(4);
+        ListNode twoOne = new ListNode(2, fourOne);
+        ListNode oneOne = new ListNode(1, twoOne);
+
+        ListNode fourTwo = new ListNode(4);
+        ListNode threeTwo = new ListNode(3, fourTwo);
+        ListNode oneTwo = new ListNode(1, threeTwo);
+
+        mergeTwoLists2(oneTwo, oneOne);
+    }
 
     // compare the head
     // for node that have smaller value, give the head to the merged list
@@ -23,6 +51,25 @@ public class MergeTwoSortedListsTest {
             merged.next = mergeTwoLists(list1, list2.next);
         }
         return merged;
+    }
+
+    private ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        ListNode merged = new ListNode(-1);
+        ListNode pointer = merged;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                pointer.next = list1;
+                list1 = list1.next;
+            } else {
+                pointer.next = list2;
+                list2 = list2.next;
+            }
+            pointer = pointer.next;
+        }
+
+        pointer.next = list1 == null ? list2 : list1;
+
+        return merged.next;
     }
 
     class ListNode {
