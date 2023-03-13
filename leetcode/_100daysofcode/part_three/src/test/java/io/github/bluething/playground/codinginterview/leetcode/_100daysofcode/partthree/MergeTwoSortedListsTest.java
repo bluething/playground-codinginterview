648eb0361af216fca3e4ae2f4f19a3845a587748
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-13 14:22:33 +0700

    Merge Two Sorted Lists problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MergeTwoSortedListsTest.java
@@ -0,0 +1,22 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+public class MergeTwoSortedListsTest {
+
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        return null;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
