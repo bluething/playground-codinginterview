2281ebdf972f33c53fe7af018c522faeddc20561
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-16 11:31:42 +0700

    Two Sum II - Input Array Is Sorted 2nd solution
    Using Map to hold pairs, it will reduce nested loop

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/TwoSumIIInputArrayIsSortedTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/TwoSumIIInputArrayIsSortedTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSumIIInputArrayIsSortedTest {
 
     @Test
@@ -20,6 +23,21 @@ public class TwoSumIIInputArrayIsSortedTest {
         Assertions.assertArrayEquals(new int[]{1,2}, twoSum(new int[]{-1,0}, -1));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertArrayEquals(new int[]{1,2}, twoSum2(new int[]{2,7,11,15}, 9));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertArrayEquals(new int[]{1,3}, twoSum2(new int[]{2,3,4}, 6));
+    }
+
+    @Test
+    void case06() {
+        Assertions.assertArrayEquals(new int[]{1,2}, twoSum2(new int[]{-1,0}, -1));
+    }
+
     private int[] twoSum(int[] numbers, int target) {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i+1; j < numbers.length; j++) {
@@ -27,6 +45,17 @@ public class TwoSumIIInputArrayIsSortedTest {
                     return new int[]{i+1, j+1};
                 }
             }
+        }
+        return new int[2];
+    }
+
+    private int[] twoSum2(int[] numbers, int target) {
+        Map<Integer, Integer> pairs = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (pairs.get(target - numbers[i]) != null) {
+                return new int[]{pairs.get(target - numbers[i])+1, i+1};
+            }
+            pairs.put(numbers[i], i);
         }
         return new int[2];
     }
