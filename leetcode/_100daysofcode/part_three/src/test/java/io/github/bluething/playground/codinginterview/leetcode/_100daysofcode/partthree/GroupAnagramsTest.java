8cfd55c9eb43c58f7cd7beadb04f34cf723526e9
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-16 11:02:57 +0700

    Group Anagram problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/GroupAnagramsTest.java
@@ -0,0 +1,34 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class GroupAnagramsTest {
+    @Test
+    public void case01() {
+        List<List<String>> expected = Arrays.asList(Arrays.asList("eat","tea","ate"),
+                Arrays.asList("bat"),
+                Arrays.asList("tan", "nat"));
+        Assertions.assertEquals(expected, groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+    }
+
+    @Test
+    public void case02() {
+        List<List<String>> expected = Arrays.asList(Arrays.asList(""));
+        Assertions.assertEquals(expected, groupAnagrams(new String[]{""}));
+    }
+
+    @Test
+    public void case03() {
+        List<List<String>> expected = Arrays.asList(Arrays.asList("a"));
+        Assertions.assertEquals(expected, groupAnagrams(new String[]{"a"}));
+    }
+
+    private List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagrams = new HashMap<>();
+
+        return new ArrayList<>(anagrams.values());
+    }
+}
