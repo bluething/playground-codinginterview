b6efeab8a376e0727bbcb1aea357b3d6eb5578cf
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-19 16:27:49 +0700

    Maximum Perimeter Triangle solution

--- hackerrank/_3monthpreparationkit/week_three/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekthree/MaximumPerimeterTriangleTest.java
+++ hackerrank/_3monthpreparationkit/week_three/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekthree/MaximumPerimeterTriangleTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 // https://www.hackerrank.com/challenges/three-month-preparation-kit-maximum-perimeter-triangle/problem
@@ -30,7 +31,20 @@ public class MaximumPerimeterTriangleTest {
         )));
     }
 
+    // Non-degenerate triangle it is a triangle that has a positive area.
+    // The condition for a non-degenerate triangle with sides a, b, c is
+    // a + b > c
+    // a + c > b
+    // b + c > a
+    // If a-b-c sorted desc, we just check if
+    // c < b + a
     private List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
-        return null;
+        sticks.sort(Comparator.reverseOrder());
+        for (int i = 0; i < sticks.size()-2; i++) {
+            if (sticks.get(i) < sticks.get(i+1) + sticks.get(i+2)) {
+                return List.of(sticks.get(i+2), sticks.get(i+1), sticks.get(i));
+            }
+        }
+        return List.of(-1);
     }
 }
