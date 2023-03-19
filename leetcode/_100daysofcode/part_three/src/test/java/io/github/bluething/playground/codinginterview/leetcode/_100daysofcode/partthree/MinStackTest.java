4ae7077deb7c0e3b899dc8de4d8b8ee2a1472425
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-19 22:13:50 +0700

    Min stack solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MinStackTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MinStackTest.java
@@ -1,27 +1,50 @@
 package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+// https://leetcode.com/problems/min-stack/
 public class MinStackTest {
 
     class MinStack {
+
+        private Node head;
 
         public MinStack() {
 
         }
 
         public void push(int val) {
-
+            if (head == null) {
+                head = new Node(val, val, null);
+            } else {
+                head = new Node(val, Math.min(head.min, val), head);
+            }
         }
 
         public void pop() {
-
+            head = head.next;
         }
 
         public int top() {
-
+            return head.val;
         }
 
         public int getMin() {
+            return head.min;
+        }
 
+        private class Node {
+            private final int val;
+            private int min;
+            Node next;
+
+            Node(int val, int min, Node next) {
+                this.val = val;
+                this.min = min;
+                this.next = next;
+            }
         }
     }
 }
