753e649bcad86919e5487009ff16d9bc1833d444
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-19 16:13:50 +0700

    Zig Zag Sequence solution

--- hackerrank/_3monthpreparationkit/week_three/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekthree/ZigZagSequenceTest.java
+++ hackerrank/_3monthpreparationkit/week_three/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekthree/ZigZagSequenceTest.java
@@ -21,21 +21,23 @@ public class ZigZagSequenceTest extends ParentTest {
         Assertions.assertArrayEquals(new int[]{1, 2, 3, 7, 6, 5, 4}, input);
     }
 
+    // Just sort the array then swap mid and tail.
+    // Don't paste the code to hackerrank editor, need to edit in place.
     private void findZigZagSequence(int [] a, int n){
         Arrays.sort(a);
-        int mid = (n + 1)/2;
+        int mid = n/2;
         int temp = a[mid];
         a[mid] = a[n - 1];
         a[n - 1] = temp;
 
         int st = mid + 1;
-        int ed = n - 1;
+        int ed = n - 2;
         while(st <= ed){
             temp = a[st];
             a[st] = a[ed];
             a[ed] = temp;
             st = st + 1;
-            ed = ed + 1;
+            ed = ed - 1;
         }
         for(int i = 0; i < n; i++){
             if(i > 0) System.out.print(" ");
