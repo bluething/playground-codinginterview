124b99d4ff04e104349619db91608d9cfda0e1f9
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-19 14:16:47 +0700

    Sales by Match problem

---
+++ hackerrank/_3monthpreparationkit/week_three/src/test/java/io/github/bluething/playground/codinginterview/hackerrank/_3monthpreparationkit/weekthree/SalesByMatchTest.java
@@ -0,0 +1,24 @@
+package io.github.bluething.playground.codinginterview.hackerrank._3monthpreparationkit.weekthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-sock-merchant/problem
+public class SalesByMatchTest {
+    @Test
+    public void case01() {
+        Assertions.assertEquals(3, sockMerchant(9, Arrays.asList(10, 20, 20, 10, 10, 30, 50, 10, 20)));
+    }
+
+    @Test
+    public void case02() {
+        Assertions.assertEquals(2, sockMerchant(7, Arrays.asList(1, 2, 1, 2, 1, 3, 2)));
+    }
+
+    private int sockMerchant(int n, List<Integer> ar) {
+        return 0;
+    }
+}
