8913d3a4064a01c45a7e87461fc1056a510480e6
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-19 17:25:23 +0700

    Min Stack problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MinStackTest.java
@@ -0,0 +1,27 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+public class MinStackTest {
+
+    class MinStack {
+
+        public MinStack() {
+
+        }
+
+        public void push(int val) {
+
+        }
+
+        public void pop() {
+
+        }
+
+        public int top() {
+
+        }
+
+        public int getMin() {
+
+        }
+    }
+}
