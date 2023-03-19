8d06241c2ca1dc500d0e0829528717e0c04ac342
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-19 16:42:26 +0700

    Fix sonar lint issues

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithoutRepeatingCharactersTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -4,34 +4,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
-public class LongestSubstringWithoutRepeatingCharactersTest {
+class LongestSubstringWithoutRepeatingCharactersTest {
     @Test
-    public void case01() {
+    void case01() {
         Assertions.assertEquals(3, lengthOfLongestSubstring("abcabcbb"));
     }
 
     @Test
-    public void case02() {
+    void case02() {
         Assertions.assertEquals(1, lengthOfLongestSubstring("bbbbb"));
     }
 
     @Test
-    public void case03() {
+    void case03() {
         Assertions.assertEquals(3, lengthOfLongestSubstring("pwwkew"));
     }
 
     @Test
-    public void case04() {
+    void case04() {
         Assertions.assertEquals(0, lengthOfLongestSubstring(""));
     }
 
     @Test
-    public void case05() {
+    void case05() {
         Assertions.assertEquals(4, lengthOfLongestSubstring("dabcabcbb"));
     }
 
     @Test
-    public void case06() {
+    void case06() {
         Assertions.assertEquals(3, lengthOfLongestSubstring("abccabcbb"));
     }
 
