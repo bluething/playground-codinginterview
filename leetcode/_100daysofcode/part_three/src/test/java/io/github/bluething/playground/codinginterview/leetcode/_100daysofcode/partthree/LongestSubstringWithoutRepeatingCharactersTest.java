522071101a24bd9ee6f7b30ad950536e6085aef1
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-19 17:35:58 +0700

    Longest Substring Without Repeating Characters solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithoutRepeatingCharactersTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 class LongestSubstringWithoutRepeatingCharactersTest {
     @Test
@@ -36,6 +39,22 @@ class LongestSubstringWithoutRepeatingCharactersTest {
     }
 
     private int lengthOfLongestSubstring(String s) {
-        return 0;
+        int maxLength = 0;
+        int length = 0;
+        char[] sChar = s.toCharArray();
+        Set<Character> uniqueChar = new HashSet<>();
+        for (int i = 0; i < sChar.length; i++) {
+            uniqueChar = new HashSet<>();
+            length = 0;
+            for (int j = i; j < sChar.length; j++) {
+                if (uniqueChar.add(sChar[j])) {
+                    length += 1;
+                } else {
+                    break;
+                }
+            }
+            maxLength = Math.max(maxLength, length);
+        }
+        return maxLength;
     }
 }
