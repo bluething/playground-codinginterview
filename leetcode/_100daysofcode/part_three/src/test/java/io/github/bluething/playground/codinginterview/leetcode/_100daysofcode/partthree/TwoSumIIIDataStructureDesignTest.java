814097811c7367f58fb30f1a644b9561a932a179
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-22 14:59:27 +0700

    Two Sum III - Data structure design 2nd solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/TwoSumIIIDataStructureDesignTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/TwoSumIIIDataStructureDesignTest.java
@@ -3,7 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 // https://aaronice.gitbook.io/lintcode/high_frequency/two-sum-iii-data-structure-design
@@ -29,7 +31,27 @@ public class TwoSumIIIDataStructureDesignTest {
         Assertions.assertFalse(twoSum.find(6));
     }
 
-    class TwoSum {
+    @Test
+    void case03() {
+        TwoSum2 twoSum = new TwoSum2();
+        twoSum.add(1);
+        twoSum.add(3);
+        twoSum.add(5);
+        Assertions.assertTrue(twoSum.find(4));
+        Assertions.assertFalse(twoSum.find(7));
+    }
+
+    @Test
+    void case04() {
+        TwoSum2 twoSum = new TwoSum2();
+        twoSum.add(3);
+        twoSum.add(1);
+        twoSum.add(2);
+        Assertions.assertTrue(twoSum.find(3));
+        Assertions.assertFalse(twoSum.find(6));
+    }
+
+    static class TwoSum {
 
         private final HashMap<Integer, Integer> valueCounter;
 
@@ -46,6 +68,51 @@ public class TwoSumIIIDataStructureDesignTest {
                 if ((pair == valueMap.getKey() && valueMap.getValue() > 1)
                         || (pair != valueMap.getKey() && valueCounter.get(pair) != null)) {
                     return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    static class TwoSum2 {
+
+        private final HashMap<Integer, Boolean> valueFlag;
+        private final List<Integer> nums;
+        private int low;
+        private int high;
+
+        TwoSum2() {
+            valueFlag = new HashMap<>();
+            nums = new ArrayList<>();
+            low = Integer.MAX_VALUE;
+            high = Integer.MIN_VALUE;
+        }
+
+        public void add(int number) {
+            if (valueFlag.get(number) != null) {
+                valueFlag.put(number, true);
+            } else {
+                valueFlag.put(number, false);
+                nums.add(number);
+                low = Math.min(low, number);
+                high = Math.max(high, number);
+            }
+        }
+
+        public boolean find(int value) {
+            if (value < 2 * low
+                    || value > 2 * high) {
+                return false;
+            }
+            for (int num : nums) {
+                int pair = value - num;
+                if (valueFlag.get(pair) != null) {
+                    if (pair != num) {
+                        return true;
+                    } else if (valueFlag.get(pair)){
+                        return true;
+                    }
                 }
             }
 
