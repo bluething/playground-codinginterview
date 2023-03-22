f1f61ec92fe3dcd5f67ec42f1d914e91bd69ef48
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-22 16:37:11 +0700

    Merge Two 2D Arrays by Summing Values problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MergeTwo2DArraysBySummingValuesTest.java
@@ -0,0 +1,46 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+class MergeTwo2DArraysBySummingValuesTest {
+
+    @Test
+    void case01() {
+        int[][] nums1 = new int[][]{{1,2},
+                {2,3},
+                {4,5}};
+        int[][] nums2 = new int[][]{{1,4},
+                {3,2},
+                {4,1}};
+        int[][] result = new int[][]{{1,6},
+                {2,3},
+                {3,2},
+                {4,6}};
+        Assertions.assertArrayEquals(result, mergeArrays(nums1, nums2));
+    }
+
+    @Test
+    void case02() {
+        int[][] nums1 = new int[][]{{2,4},
+                {3,6},
+                {5,5}};
+        int[][] nums2 = new int[][]{{1,3},
+                {4,3}};
+        int[][] result = new int[][]{{1,3},
+                {2,4},
+                {3,6},
+                {4,3},
+                {5,5}};
+        Assertions.assertArrayEquals(result, mergeArrays(nums1, nums2));
+    }
+
+    private int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        return null;
+    }
+}
