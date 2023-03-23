93f2c438c57fc95030cf935276ec5f776a31fa31
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-23 08:08:30 +0700

    Power of Three problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/PowerOfThreeTest.java
@@ -0,0 +1,27 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/power-of-three/description/
+class PowerOfThreeTest {
+
+    @Test
+    void case01() {
+        Assertions.assertTrue(isPowerOfThree(27));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertTrue(isPowerOfThree(0));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertTrue(isPowerOfThree(-1));
+    }
+
+    private boolean isPowerOfThree(int n) {
+        return false;
+    }
+}
