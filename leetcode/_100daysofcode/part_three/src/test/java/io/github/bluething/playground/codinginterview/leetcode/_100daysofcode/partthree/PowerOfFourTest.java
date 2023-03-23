d8c8a6670d1fc3a8bf413d5c89dc230cd7e8abad
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-23 08:10:28 +0700

    Power of Four problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/PowerOfFourTest.java
@@ -0,0 +1,27 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/power-of-four/description/
+class PowerOfFourTest {
+
+    @Test
+    void case01() {
+        Assertions.assertTrue(isPowerOfFour(16));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertFalse(isPowerOfFour(5));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertTrue(isPowerOfFour(1));
+    }
+
+    private boolean isPowerOfFour(int n) {
+        return false;
+    }
+}
