6d5c35bb02614bef6031be5d9cac3be7f1213124
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-23 13:11:59 +0700

    Nth Fibo problem

---
+++ concept/recursion/src/test/java/io/github/bluething/playground/codinginterview/concept/recursion/NthFiboTest.java
@@ -0,0 +1,16 @@
+package io.github.bluething.playground.codinginterview.concept.recursion;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class NthFiboTest {
+
+    @Test
+    void case01() {
+        Assertions.assertEquals(8, fib(5));
+    }
+
+    private int fib(int n) {
+        return 0;
+    }
+}
