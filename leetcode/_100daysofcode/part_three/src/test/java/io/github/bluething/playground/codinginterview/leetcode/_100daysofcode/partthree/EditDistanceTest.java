af96d4490cbb9e45b6ad0b7dfa80a173593ef957
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-23 13:39:03 +0700

    Edit Distance 1st solution
    Hit time limit exceed

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/EditDistanceTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/EditDistanceTest.java
@@ -16,7 +16,29 @@ class EditDistanceTest {
         Assertions.assertEquals(5, minDistance("intention", "execution"));
     }
 
+    @Test
+    void case03() {
+        Assertions.assertEquals(0, minDistance("dinitrophenylhydrazine", "benzalphenylhydrazone"));
+    }
+
     private int minDistance(String word1, String word2) {
-        return 0;
+        return editDistance(word1, word2, word1.length(), word2.length());
+    }
+
+    private int editDistance(String word1, String word2, int i, int j) {
+        if (i == 0) {
+            return j;
+        }
+        if (j == 0) {
+            return i;
+        }
+        if (word1.charAt(i-1) == word2.charAt(j-1)) {
+            return editDistance(word1, word2, i-1, j-1);
+        }
+
+        return 1 + Math.min(
+                Math.min(editDistance(word1, word2, i, j-1)
+                        , editDistance(word1, word2, i-1, j))
+                , editDistance(word1, word2, i-1, j-1));
     }
 }
