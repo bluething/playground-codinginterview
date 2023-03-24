0cf288d42671c2c0d0858b885cce327d8139b517
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-25 01:56:19 +0700

    Longest Substring with At Most Two Distinct Characters solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithAtMostTwoDistinctCharactersTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithAtMostTwoDistinctCharactersTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://aaronice.gitbook.io/lintcode/two_pointers/longest-substring-with-at-most-two-distinct-characters
 class LongestSubstringWithAtMostTwoDistinctCharactersTest {
     @Test
@@ -16,6 +19,27 @@ class LongestSubstringWithAtMostTwoDistinctCharactersTest {
     }
 
     private int lengthOfLongestSubstringTwoDistinct(String s) {
-        return 0;
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] sChars = s.toCharArray();
+        Map<Character, Integer> charFreq = new HashMap<>();
+        int leftIdx = 0;
+        int longest = 0;
+        for (int i = 0; i < s.length(); i++) {
+            charFreq.put(sChars[i], charFreq.getOrDefault(sChars[i], 0) + 1);
+            // find 1st char with single occurrences in the window
+            // the position (leftIdx) is the candidate for 1st char in our longest substring
+            while (charFreq.size() > 2) {
+                charFreq.put(sChars[leftIdx], charFreq.get(sChars[leftIdx]) -1);
+                if (charFreq.get(sChars[leftIdx]) == 0) {
+                    charFreq.remove(sChars[leftIdx]);
+                }
+                leftIdx++;
+            }
+            longest = Math.max(longest, i - leftIdx + 1);
+        }
+        return longest;
     }
 }
