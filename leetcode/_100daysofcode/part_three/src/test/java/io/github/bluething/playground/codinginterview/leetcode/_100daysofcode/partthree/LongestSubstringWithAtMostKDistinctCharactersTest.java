7807022ec4948d6befa45c738c9505d0b5331d58
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-25 01:54:43 +0700

    Longest Substring with At Most K Distinct Characters solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithAtMostKDistinctCharactersTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithAtMostKDistinctCharactersTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://aaronice.gitbook.io/lintcode/two_pointers/longest_substring_with_at_most_k_distinct_characters
 class LongestSubstringWithAtMostKDistinctCharactersTest {
 
@@ -12,6 +15,27 @@ class LongestSubstringWithAtMostKDistinctCharactersTest {
     }
 
     private int lengthOfLongestSubstringKDistinct(String s, int k) {
-        return 0;
+        if (s == null || s.length() == 0 || k == 0) {
+            return 0;
+        }
+
+        char[] sChars = s.toCharArray();
+        Map<Character, Integer> charFreq = new HashMap<>();
+        int leftIdx = 0;
+        int longest = 0;
+        for (int i = 0; i < s.length(); i++) {
+            charFreq.put(sChars[i], charFreq.getOrDefault(sChars[i], 0) + 1);
+            // find 1st char with single occurrences in the window
+            // the position (leftIdx) is the candidate for 1st char in our longest substring
+            while (charFreq.size() > k) {
+                charFreq.put(sChars[leftIdx], charFreq.get(sChars[leftIdx]) -1);
+                if (charFreq.get(sChars[leftIdx]) == 0) {
+                    charFreq.remove(sChars[leftIdx]);
+                }
+                leftIdx++;
+            }
+            longest = Math.max(longest, i - leftIdx + 1);
+        }
+        return longest;
     }
 }
