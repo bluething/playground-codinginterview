2d646fcc0fc9fd5796fbcf88b209800033479f0d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-24 22:59:59 +0700

    Longest Substring with At Most K Distinct Characters problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestSubstringWithAtMostKDistinctCharactersTest.java
@@ -0,0 +1,17 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://aaronice.gitbook.io/lintcode/two_pointers/longest_substring_with_at_most_k_distinct_characters
+class LongestSubstringWithAtMostKDistinctCharactersTest {
+
+    @Test
+    void case01() {
+        Assertions.assertEquals(4, lengthOfLongestSubstringKDistinct("eceba", 3));
+    }
+
+    private int lengthOfLongestSubstringKDistinct(String s, int k) {
+        return 0;
+    }
+}
