b77b494178355be0fc47a1d97e00fdb1cc29b0ed
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-29 23:57:54 +0700

    Tower of hanoi solution

--- concept/recursion/src/test/java/io/github/bluething/playground/codinginterview/concept/recursion/TowerOfHanoiTest.java
+++ concept/recursion/src/test/java/io/github/bluething/playground/codinginterview/concept/recursion/TowerOfHanoiTest.java
@@ -1,6 +1,28 @@
 package io.github.bluething.playground.codinginterview.concept.recursion;
 
+import org.junit.jupiter.api.Test;
+
 // https://www.hackerearth.com/blog/developers/tower-hanoi-recursion-game-algorithm-explained/
 // https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/
-public class TowerOfHanoiTest {
+class TowerOfHanoiTest {
+
+    @Test
+    void case01() {
+        doMove(3, 'a', 'c', 'b');
+    }
+
+    @Test
+    void case02() {
+        doMove(6, 'a', 'c', 'b');
+    }
+
+    private void doMove(int n, char from, char to, char helper) {
+        if (n == 0) {
+            return;
+        }
+
+        doMove(n-1, from, helper, to);
+        System.out.printf("Move disk %d from %c to %c\n", n, from, to);
+        doMove(n-1, helper, to, from);
+    }
 }
