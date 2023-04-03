c40fae0180cfc9f837d148393b6c27f2b53a808c
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-03 23:17:19 +0700

    Letter Combinations of a Phone Number solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LetterCombinationsOfAPhoneNumberTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LetterCombinationsOfAPhoneNumberTest.java
@@ -3,6 +3,7 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,6 +25,28 @@ class LetterCombinationsOfAPhoneNumberTest {
         Assertions.assertEquals(List.of("a","b","c"), letterCombinations("2"));
     }
     private List<String> letterCombinations(String digits) {
-        return null;
+        List<String> results = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return results;
+        }
+
+        String[] letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        generateCombination(results, new StringBuilder(), letters, digits, 0);
+        return results;
+    }
+
+    private void generateCombination(List<String> results, StringBuilder sb, String[] letters, String digits, int idx) {
+        if (idx == digits.length()) {
+            results.add(sb.toString());
+            return;
+        }
+
+        int digit = digits.charAt(idx) - '0';
+        String letter = letters[digit - 2];
+        for (int i = 0; i < letter.length(); i++) {
+            sb.append(letter.charAt(i));
+            generateCombination(results, sb, letters, digits, idx + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 }
