2fbd764dbf2061c14c74aa8fc1fc2a80ec7c454d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-04 00:08:30 +0700

    Check if a Parentheses String Can Be Valid solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/CheckIfAParenthesesStringCanBeValidTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/CheckIfAParenthesesStringCanBeValidTest.java
@@ -21,7 +21,47 @@ class CheckIfAParenthesesStringCanBeValidTest {
         Assertions.assertFalse(canBeValid(")", "0"));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertFalse(canBeValid("))((", "0011"));
+    }
+
     private boolean canBeValid(String s, String locked) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        int unlockedCount = 0;
+        int openLockedCount = 0;
+        int closeLockedCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (locked.charAt(i) == '0') {
+                unlockedCount++;
+            } else if (s.charAt(i) == '(') {
+                openLockedCount++;
+            } else if (s.charAt(i) == ')'){
+                closeLockedCount++;
+            }
+
+            if (unlockedCount + openLockedCount - closeLockedCount < 0) {
+                return false;
+            }
+        }
+
+        unlockedCount = openLockedCount = closeLockedCount = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (locked.charAt(i) == '0') {
+                unlockedCount++;
+            } else if (s.charAt(i) == '(') {
+                openLockedCount++;
+            } else if (s.charAt(i) == ')'){
+                closeLockedCount++;
+            }
+
+            if (unlockedCount - openLockedCount + closeLockedCount < 0) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
