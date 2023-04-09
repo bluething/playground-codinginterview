690eb68e7912a5ebbe259e2a49d8c8f846397c0e
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-09 22:46:45 +0700

    Delete N nodes after M nodes of a linked list problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DeleteNNodesAfterMNodesOfALinkedListTest.java
@@ -0,0 +1,25 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+// https://www.geeksforgeeks.org/delete-n-nodes-after-m-nodes-of-a-linked-list/
+public class DeleteNNodesAfterMNodesOfALinkedListTest {
+    private void skipMdeleteN( ListNode head, int m, int n) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
