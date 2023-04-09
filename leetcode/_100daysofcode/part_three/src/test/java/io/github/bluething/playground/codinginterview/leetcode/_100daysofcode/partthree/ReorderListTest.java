37a5abe9ac2eec8d92dff42ca295fa2f8435899d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-09 22:55:24 +0700

    Reorder List problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ReorderListTest.java
@@ -0,0 +1,25 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+// https://leetcode.com/problems/reorder-list/
+class ReorderListTest {
+    private void reorderList(ListNode head) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
