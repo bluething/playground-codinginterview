d27fed1088f79954d2aa6fcd535ccf67288419f9
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-09 22:51:10 +0700

    Design HashSet problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DesignHashSetTest.java
@@ -0,0 +1,24 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+// https://leetcode.com/problems/design-hashset/
+class DesignHashSetTest {
+
+    class MyHashSet {
+
+        public MyHashSet() {
+
+        }
+
+        public void add(int key) {
+
+        }
+
+        public void remove(int key) {
+
+        }
+
+        public boolean contains(int key) {
+
+        }
+    }
+}
