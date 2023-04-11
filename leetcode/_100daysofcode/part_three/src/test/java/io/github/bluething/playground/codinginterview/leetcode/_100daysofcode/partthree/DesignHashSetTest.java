11765d43bfc5a64c51d38848540e12a36933f2a9
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-11 23:45:04 +0700

    Design HashSet 2nd solution
    Backed by linked list

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DesignHashSetTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DesignHashSetTest.java
@@ -23,4 +23,75 @@ class DesignHashSetTest {
             return data[key];
         }
     }
+
+    class MyHashSet2 {
+
+        class Node {
+            Node next;
+            int val;
+            public Node(int key) {
+                this.val = key;
+                this.next = null;
+            }
+        }
+
+        Node head;
+
+        public MyHashSet2() {
+            head = null;
+        }
+
+        public void add(int key) {
+            if (head == null) {
+                head = new Node(key);
+                return;
+            }
+
+            boolean isExist = contains(key);
+            if (!isExist) {
+                Node tmp = head;
+                while (tmp.next != null) {
+                    tmp = tmp.next;
+                }
+                tmp.next = new Node(key);
+            }
+        }
+
+        public void remove(int key) {
+            if (head == null) {
+                return;
+            }
+            if (head.val == key) {
+                head = head.next;
+                return;
+            }
+            Node tmp = head;
+            while (tmp.next != null) {
+                if (tmp.next.val == key) {
+                    tmp.next = tmp.next.next;
+                } else {
+                    tmp = tmp.next;
+                }
+            }
+        }
+
+        public boolean contains(int key) {
+            if (head == null) {
+                return false;
+            }
+            if (head.val == key) {
+                return true;
+            }
+            Node tmp = head;
+            while (tmp.next != null) {
+                if (tmp.next.val == key) {
+                    return true;
+                } else {
+                    tmp = tmp.next;
+                }
+            }
+
+            return false;
+        }
+    }
 }
