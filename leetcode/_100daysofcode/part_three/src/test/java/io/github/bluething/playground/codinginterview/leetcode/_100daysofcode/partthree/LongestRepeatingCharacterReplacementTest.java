3168fc8fcb4c854e84aed54b894008c28499618d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-11 13:24:18 +0700

    Add some solution description

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestRepeatingCharacterReplacementTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/LongestRepeatingCharacterReplacementTest.java
@@ -15,6 +15,10 @@ class LongestRepeatingCharacterReplacementTest {
         Assertions.assertEquals(4, characterReplacement("AABABBA", 1));
     }
 
+    // the key is we need to define a valid substring that the chars can be replaced at most k
+    // to achieve that we need to have a counter, we need to know the most frequent char in current substring
+    // so the longest valid substring can be defined by
+    // window length - most frequent char <= k
     private int characterReplacement(String s, int k) {
         int[] sCharFreq = new int[26];
         int left = 0;
@@ -22,7 +26,17 @@ class LongestRepeatingCharacterReplacementTest {
         int longestRepeatedChar = 0;
         for (int right = 0; right < s.length(); right++) {
             sCharFreq[s.charAt(right)-'A']++;
+            // no need to decrease the value
+            // we will get the new longest substring when we meet a new maxFreq value
+            // from this equation window length - most frequent char <= k
+            // we will have longest substring if we can maximize window length
+            // but we need to have max most frequent char also to keep the condition <= k
             maxFreq = Math.max(maxFreq, sCharFreq[s.charAt(right)-'A']);
+            // maintain the window
+            // we don't want to have a window that non-frequent char appear more that window length - k
+            // because we have to replace it maximum as much k
+            // if we reach the size, move the left pointer to the right
+            // we need to decrease the freq char
             if ((right - left + 1) - maxFreq > k) {
                 sCharFreq[s.charAt(left)-'A']--;
                 left++;
