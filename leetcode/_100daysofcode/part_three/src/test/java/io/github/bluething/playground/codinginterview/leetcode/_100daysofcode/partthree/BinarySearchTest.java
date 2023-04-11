dec729216e90f446c3394e2eca354ad7a3b1f783
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-12 06:48:49 +0700

    Binary Search 2nd solution
    Recursive

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/BinarySearchTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/BinarySearchTest.java
@@ -16,6 +16,16 @@ class BinarySearchTest {
         Assertions.assertEquals(-1, search(new int[]{-1,0,3,5,9,12}, 2));
     }
 
+    @Test
+    void case03() {
+        Assertions.assertEquals(4, search2(new int[]{-1,0,3,5,9,12}, 9));
+    }
+
+    @Test
+    void case04() {
+        Assertions.assertEquals(-1, search2(new int[]{-1,0,3,5,9,12}, 2));
+    }
+
     private int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length-1;
@@ -31,5 +41,22 @@ class BinarySearchTest {
             }
         }
         return -1;
+    }
+
+    private int search2(int[] nums, int target) {
+        return binarySearchRec(nums, target, 0, nums.length-1);
+    }
+    private int binarySearchRec(int[] nums, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (right + left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return binarySearchRec(nums, target, mid + 1, right);
+        } else {
+            return binarySearchRec(nums, target, left, mid-1);
+        }
     }
 }
