dcdbc3279856de97fdaeb99ce3d055320e0a4ca4
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-14 00:20:15 +0700

    Substrings of Size Three with Distinct Characters solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/SubstringsOfSizeThreeWithDistinctCharactersTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/SubstringsOfSizeThreeWithDistinctCharactersTest.java
@@ -17,6 +17,31 @@ class SubstringsOfSizeThreeWithDistinctCharactersTest {
     }
 
     private int countGoodSubstrings(String s) {
-        return 0;
+        int[] freq = new int[128];
+        int repeat = 0;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i)] == 1) {
+                repeat++;
+            }
+            freq[s.charAt(i)]++;
+            // keep the window size (3)
+            // start from 4th position
+            // we must check if the 1st position hava 2 freq
+            // if yes, decrement by 1
+            // we don't allow inside the window have freq=2 (repeat)
+            if (i >= 3) {
+                if (freq[s.charAt(i-3)] == 2) {
+                    repeat--;
+                }
+                freq[s.charAt(i-3)]--;
+            }
+            // inside the window
+            // check if we have repeated char
+            if (i >= 2 && repeat == 0) {
+                result++;
+            }
+        }
+        return result;
     }
 }
