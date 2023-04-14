000c8927f3c2c9c1b13b462ed05ed7e8edb26c12
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-15 00:20:12 +0700

    Valid Parenthesis String 1st solution
    Using 2 stack

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ValidParenthesisStringTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/ValidParenthesisStringTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 // https://leetcode.com/problems/valid-parenthesis-string/description/
 class ValidParenthesisStringTest {
 
@@ -21,7 +23,40 @@ class ValidParenthesisStringTest {
         Assertions.assertTrue(checkValidString("(*))"));
     }
 
+    // push open to stack
+    // find close bracket
+    //// pop open stack if not empty, case ()
+    //// pop star stack if not empty, case *)
+    //// otherwise return false, case )
+    // push star to stack
+    // we need to make sure open stack if empty because * can be use as empty
+    // until both open and star stack empty we need to check if the idx of open is before star, case (*, * is close bracket
     private boolean checkValidString(String s) {
-        return false;
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+        for (int i = 0; i<s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                open.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!open.isEmpty()) {
+                    open.pop();
+                } else if (!star.isEmpty()) {
+                    star.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                star.push(i);
+            }
+        }
+
+        while (!open.isEmpty() && !star.isEmpty()) {
+            int idx = star.pop();
+            if (open.peek() < idx) {
+                open.pop();
+            }
+        }
+
+        return open.isEmpty();
     }
 }
