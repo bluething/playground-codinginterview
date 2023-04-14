92034319cace6cdff8d4feda3b35d9da04cfdd82
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-15 02:42:14 +0700

    Subset solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/SubsetsTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/SubsetsTest.java
@@ -3,6 +3,7 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,7 +28,57 @@ class SubsetsTest {
                 Arrays.asList(0)), subsets(new int[]{0}));
     }
 
+    @Test
+    void case03() {
+        Assertions.assertEquals(Arrays.asList(Arrays.asList(),
+                Arrays.asList(1),
+                Arrays.asList(1,2),
+                Arrays.asList(1,2,3),
+                Arrays.asList(1,3),
+                Arrays.asList(2),
+                Arrays.asList(2,3),
+                Arrays.asList(3)), subsets2(new int[]{1,2,3}));
+    }
+
+    @Test
+    void case04() {
+        Assertions.assertEquals(Arrays.asList(Arrays.asList(),
+                Arrays.asList(0)), subsets2(new int[]{0}));
+    }
+
+    // for each num
+    //// start from 0 as much as current size
+    ////// for each
+    /////// get previous result as temp
+    /////// add current num to temp
+    /////// add temp to result
     private List<List<Integer>> subsets(int[] nums) {
-        return null;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for (int i=0; i< nums.length; i++) {
+            for (int j=0, size=result.size(); j<size; j++) {
+                List<Integer> temp = new ArrayList<>(result.get(j));
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+        }
+        return result;
+    }
+
+    private List<List<Integer>> subsets2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, int start) {
+        result.add(new ArrayList<>(temp));
+        for (int i=start; i< nums.length; i++) {
+            temp.add(nums[i]);
+            backtrack(result, temp, nums, i+1);
+            temp.remove(temp.size()-1);
+        }
     }
 }
