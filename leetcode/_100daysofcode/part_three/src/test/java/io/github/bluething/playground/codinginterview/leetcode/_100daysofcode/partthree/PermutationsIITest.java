fc7f98fee2f7bb9e53f2e73f3b8310fb1bde66fe
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-20 06:51:44 +0700

    Permutations II problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/PermutationsIITest.java
@@ -0,0 +1,32 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+// https://leetcode.com/problems/permutations-ii/
+class PermutationsIITest {
+
+    @Test
+    void case01() {
+        Assertions.assertEquals(Arrays.asList(Arrays.asList(1,2,3),
+                Arrays.asList(1,3,2),
+                Arrays.asList(2,1,3),
+                Arrays.asList(2,3,1),
+                Arrays.asList(3,1,2),
+                Arrays.asList(3,2,1)), permuteUnique(new int[]{1,2,3}));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertEquals(Arrays.asList(Arrays.asList(1,1,2),
+                Arrays.asList(1,2,1),
+                Arrays.asList(2,1,1)), permuteUnique(new int[]{1,1,2}));
+    }
+
+    private List<List<Integer>> permuteUnique(int[] nums) {
+        return null;
+    }
+}
