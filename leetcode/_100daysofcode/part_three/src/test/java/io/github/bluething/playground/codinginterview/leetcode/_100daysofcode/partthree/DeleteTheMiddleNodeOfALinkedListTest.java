a3490985e9d2329cded38b4a0cf5ac4cd2f47239
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-21 00:05:59 +0700

    Delete the Middle Node of a Linked List solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DeleteTheMiddleNodeOfALinkedListTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DeleteTheMiddleNodeOfALinkedListTest.java
@@ -39,7 +39,16 @@ class DeleteTheMiddleNodeOfALinkedListTest extends ParentTest {
     }
 
     private ListNode deleteMiddle(ListNode head) {
-        return null;
+        ListNode dummy = new ListNode(0, head);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+
+        return dummy.next;
     }
 
     void printLinkedList(ListNode head) {
