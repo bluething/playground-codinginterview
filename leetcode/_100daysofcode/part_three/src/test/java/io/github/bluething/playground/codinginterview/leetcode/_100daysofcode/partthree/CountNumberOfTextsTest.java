5e79edcf05539396caae7a3ca8bb1333679bb16e
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-21 23:57:55 +0700

    Count Number of Texts solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/CountNumberOfTextsTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/CountNumberOfTextsTest.java
@@ -17,6 +17,49 @@ class CountNumberOfTextsTest {
     }
 
     private int countTexts(String pressedKeys) {
-        return 0;
+        int[] memo = new int[pressedKeys.length()];
+
+        return dfs(pressedKeys, memo, 0);
+    }
+
+    // the key is how many ways to interpret the prefix
+    private int dfs(String pressedKeys, int[] memo, int idx) {
+        // end of string, we only hava one permutation
+        if (idx == pressedKeys.length()) {
+            return 1;
+        }
+        if (memo[idx] != 0) {
+            return memo[idx];
+        }
+
+        int count=0;
+        // the maximum prefix length that we can have is 4 (for digit 7 and 9)
+        for (int i=0; i<4; i++) {
+            // when the prefix length is 4, make sure we only process digit 7 or 9
+            if (pressedKeys.charAt(idx) != '7' && pressedKeys.charAt(idx) != '9' && i == 3) {
+                break;
+            }
+            // to avoid index out of bound
+            if (idx+i >= pressedKeys.length()) {
+                break;
+            }
+            // make sure the prefix have same char
+            // 222 can be interpreted as
+            // aaa
+            // ba
+            // c
+            if (i != 0) {
+                if (pressedKeys.charAt(idx+i) != pressedKeys.charAt(idx+i-1)) {
+                    break;
+                }
+            }
+
+            // go to the right char after prefix
+            count += dfs(pressedKeys, memo, idx+i+1);
+            count %= 1000000007;
+        }
+
+        memo[idx] = count;
+        return count;
     }
 }
