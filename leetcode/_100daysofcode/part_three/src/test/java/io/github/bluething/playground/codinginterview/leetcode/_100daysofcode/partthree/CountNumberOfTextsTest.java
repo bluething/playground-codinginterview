3f8aedb8d0976da35a2688522247484f196a350a
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-22 00:28:54 +0700

    Count Number of Texts 2nd solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/CountNumberOfTextsTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/CountNumberOfTextsTest.java
@@ -16,6 +16,16 @@ class CountNumberOfTextsTest {
         Assertions.assertEquals(82876089, countTexts("222222222222222222222222222222222222"));
     }
 
+    @Test
+    void case03() {
+        Assertions.assertEquals(8, countTexts2("22233"));
+    }
+
+    @Test
+    void case04() {
+        Assertions.assertEquals(82876089, countTexts2("222222222222222222222222222222222222"));
+    }
+
     private int countTexts(String pressedKeys) {
         int[] memo = new int[pressedKeys.length()];
 
@@ -61,5 +71,32 @@ class CountNumberOfTextsTest {
 
         memo[idx] = count;
         return count;
+    }
+
+    // bottom up, right -> left
+    private int countTexts2(String pressedKeys) {
+        int mod = 1000000007;
+        int[] freqKey = new int[]{0,0,3,3,3,3,3,4,3,4};
+        int n = pressedKeys.length();
+        int[] dp = new int[n+1];
+        // init (baseline)
+        dp[n] = 1;
+        for (int idx = n-1; idx >= 0; idx--) {
+            int count = 0;
+            int digit = pressedKeys.charAt(idx) - '0';
+            int freq = freqKey[digit];
+            // if we have prefix with same char, need to count the permutation how to interpret the prefix
+            for (int i = 0;
+                 i < freq   // check for prefix length
+                         && idx+i < pressedKeys.length()    // avoid index out of bound
+                         && pressedKeys.charAt(idx+i) == pressedKeys.charAt(idx);   // prefix must have same char
+                 i++) {
+                count += dp[idx+i+1];
+                count %= mod;
+            }
+            dp[idx] = count;
+        }
+
+        return dp[0];
     }
 }
