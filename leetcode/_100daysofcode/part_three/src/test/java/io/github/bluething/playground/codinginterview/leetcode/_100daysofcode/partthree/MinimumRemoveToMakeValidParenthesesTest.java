0d6fbcf70ab9bd0347bfa8c14631d64289778fa2
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-22 00:42:59 +0700

    Minimum Remove to Make Valid Parentheses solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MinimumRemoveToMakeValidParenthesesTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MinimumRemoveToMakeValidParenthesesTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 // https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
 class MinimumRemoveToMakeValidParenthesesTest {
 
@@ -21,7 +23,29 @@ class MinimumRemoveToMakeValidParenthesesTest {
         Assertions.assertEquals("", minRemoveToMakeValid("))(("));
     }
 
+    // we need to know which char that must be deleted
+    // using stack, push every open parentheses
+    // for each open, only pop the stack if the stack was not empty and the top value is open
+    //// we put the index, so we need to get from the string
+    // otherwise, push to the top, this is the char that we need to remove later.
     private String minRemoveToMakeValid(String s) {
-        return "";
+        Stack<Integer> removedIdx = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                removedIdx.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!removedIdx.isEmpty() && s.charAt(removedIdx.peek()) == '(') {
+                    removedIdx.pop();
+                } else {
+                    removedIdx.push(i);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder(s);
+        while (!removedIdx.isEmpty()) {
+            sb.deleteCharAt(removedIdx.pop());
+        }
+
+        return sb.toString();
     }
 }
