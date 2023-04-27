37eed1bcb9c135b9fccba5732d5810510367004b
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-27 23:39:20 +0700

    Palindrome Permutation II solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/PalindromePermutationIITest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/PalindromePermutationIITest.java
@@ -11,7 +11,7 @@ import java.util.List;
 class PalindromePermutationIITest {
     @Test
     void case01() {
-        Assertions.assertEquals(Arrays.asList("abba", "baab"), generatePalindromes("aabb"));
+        Assertions.assertEquals(Arrays.asList("baab", "abba"), generatePalindromes("aabb"));
     }
 
     @Test
@@ -20,10 +20,46 @@ class PalindromePermutationIITest {
     }
 
     private List<String> generatePalindromes(String s) {
-        
+        int[] freqChar = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            freqChar[s.charAt(i)]++;
+        }
 
         List<String> result = new ArrayList<>();
+        boolean isOdd = false;
+        int oddIdx = 0;
+        for (int i = 0; i < freqChar.length; i++) {
+            if (freqChar[i] % 2 == 1) {
+                if (isOdd) {
+                    return result;
+                }
+                oddIdx = i;
+                isOdd = true;
+            }
+        }
+
+        String base = "";
+        if (isOdd) {
+            base = (char)oddIdx + "";
+            freqChar[oddIdx]--;
+        }
+
+        backtracking(result, base, freqChar, s.length());
 
         return result;
+    }
+
+    private void backtracking(List<String> result, String base, int[] freqChar, int length) {
+        if (base.length() == length) {
+            result.add(base);
+            return;
+        }
+        for (int i = 0; i < freqChar.length; i++) {
+            if (freqChar[i] > 0) {
+                freqChar[i] -= 2;
+                backtracking(result, (char)i + base + (char)i, freqChar, length);
+                freqChar[i] += 2;
+            }
+        }
     }
 }
