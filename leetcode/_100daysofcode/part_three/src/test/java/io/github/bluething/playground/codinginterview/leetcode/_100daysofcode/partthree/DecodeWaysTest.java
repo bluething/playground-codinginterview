a62a4de037d1cb26830bef9f88a42400477c5f28
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-28 23:12:05 +0700

    Decode Ways 1st solution
    Recursive, hit TLE

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DecodeWaysTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DecodeWaysTest.java
@@ -22,6 +22,20 @@ class DecodeWaysTest {
     }
 
     private int numDecodings(String s) {
-        return 0;
+        return numDecodingsRec(s, 0);
+    }
+    private int numDecodingsRec(String s, int idx) {
+        if (idx == s.length()) {
+            return 1;
+        }
+        if (s.charAt(idx) == '0') {
+            return 0;
+        }
+        int count = numDecodingsRec(s, idx+1);
+        if (idx < s.length()-1 && ((s.charAt(idx)-'0') * 10 + (s.charAt(idx+1)-'0')) < 27) {
+            count += numDecodingsRec(s, idx+2);
+        }
+
+        return count;
     }
 }
