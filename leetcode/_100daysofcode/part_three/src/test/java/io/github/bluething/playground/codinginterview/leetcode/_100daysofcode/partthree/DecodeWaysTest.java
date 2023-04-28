ca47b22f483b16a7f3e6c06ee32f0582b0f50999
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-28 23:16:12 +0700

    Decode Ways 2nd solution
    Recursive with memoization

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DecodeWaysTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/DecodeWaysTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 // https://leetcode.com/problems/decode-ways/
 class DecodeWaysTest {
 
@@ -21,6 +23,21 @@ class DecodeWaysTest {
         Assertions.assertEquals(0, numDecodings("06"));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertEquals(2, numDecodings2("12"));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertEquals(3, numDecodings2("226"));
+    }
+
+    @Test
+    void case06() {
+        Assertions.assertEquals(0, numDecodings2("06"));
+    }
+
     private int numDecodings(String s) {
         return numDecodingsRec(s, 0);
     }
@@ -37,5 +54,28 @@ class DecodeWaysTest {
         }
 
         return count;
+    }
+
+    private int numDecodings2(String s) {
+        int[] mem = new int[s.length()];
+        Arrays.fill(mem, -1);
+        return numDecodingsRecMemo(s, 0, mem);
+    }
+    private int numDecodingsRecMemo(String s, int idx, int[] mem) {
+        if (idx == s.length()) {
+            return 1;
+        }
+        if (s.charAt(idx) == '0') {
+            return 0;
+        }
+        if (mem[idx] != -1) {
+            return mem[idx];
+        }
+        int count = numDecodingsRecMemo(s, idx+1, mem);
+        if (idx < s.length()-1 && ((s.charAt(idx)-'0') * 10 + (s.charAt(idx+1)-'0')) < 27) {
+            count += numDecodingsRecMemo(s, idx+2, mem);
+        }
+
+        return mem[idx] = count;
     }
 }
