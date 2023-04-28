232ecd495c1906ae75bf097b45b6d3c85085a9e3
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-29 00:56:11 +0700

    Minimum Number of Swaps to Make the String Balanced solution

--- leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MinimumNumberOfSwapsToMakeTheStringBalancedTest.java
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MinimumNumberOfSwapsToMakeTheStringBalancedTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 // https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/description/
 class MinimumNumberOfSwapsToMakeTheStringBalancedTest {
 
@@ -21,7 +23,58 @@ class MinimumNumberOfSwapsToMakeTheStringBalancedTest {
         Assertions.assertEquals(0, minSwaps("[]"));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertEquals(1, minSwaps2("][]["));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertEquals(2, minSwaps2("]]][[["));
+    }
+
+    @Test
+    void case06() {
+        Assertions.assertEquals(0, minSwaps2("[]"));
+    }
+
+    // keep track of unbalance close
+    // each swap will reduce unbalance close by 2
+    // ] ] ] [ [ [ [ ]
+    // 0 1 2 3 4 5 6 7
+    // we have 3 unbalance close 0-1-2
+    // we can swap 3 times 0->3 1->4 2->5
+    // but for minimum swap, we only need to swap 0-6 and 1-5 (2 swap)
+    // [ [ ] [ [ ] ] ] => balance
     private int minSwaps(String s) {
-        return 0;
+        Stack<Character> brackets = new Stack<>();
+        int unbalance = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '[') {
+                brackets.push(c);
+            } else {
+                if (brackets.isEmpty()) {
+                    unbalance++;
+                } else {
+                    brackets.pop();
+                }
+            }
+        }
+        return (unbalance+1) / 2;
+    }
+
+    private int minSwaps2(String s) {
+        int unbalance = 0;
+        int close = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '[') {
+                close--;
+            } else {
+                close++;
+            }
+            unbalance = Math.max(close, unbalance);
+        }
+
+        return (unbalance+1) / 2;
     }
 }
