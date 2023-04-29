b6ddcf7832fdbbafbb8f06569d2f742cd914fad4
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-29 23:05:04 +0700

    Init atcoder

---
+++ atcoder/atcoder-beginner/src/main/java/io/github/bluething/playground/codinginterview/atcoder/Main.java
@@ -0,0 +1,18 @@
+package io.github.bluething.playground.codinginterview.atcoder;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // get a integer
+        int a = sc.nextInt();
+        // get two integers separated with half-width break
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // get a string
+        String s = sc.next();
+        // output
+        System.out.println((a+b+c) + " " + s);
+    }
+}
