a30b2a87ec32a6637b8f1289ecdfb9a40e6cc32b
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-02 11:02:20 +0700

    Maximum Depth of Binary Tree problem

---
+++ leetcode/_100daysofcode/part_three/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partthree/MaximumDepthOfBinaryTreeTest.java
@@ -0,0 +1,35 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.partthree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+class MaximumDepthOfBinaryTreeTest {
+
+    @Test
+    void case01() {
+        TreeNode input7 = new TreeNode(9);
+        TreeNode input15 = new TreeNode(6);
+        TreeNode input20 = new TreeNode(7, input15, input7);
+        TreeNode input9 = new TreeNode(1);
+        TreeNode input3 = new TreeNode(3, input9, input20);
+        Assertions.assertEquals(3, maxDepth(input3));
+    }
+
+    private int maxDepth(TreeNode root) {
+        return 0;
+    }
+
+    class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.value = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.value = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
