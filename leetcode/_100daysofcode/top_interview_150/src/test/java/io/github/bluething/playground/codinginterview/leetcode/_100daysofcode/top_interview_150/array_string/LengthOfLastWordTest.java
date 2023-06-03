5fc8fe1d36b55c1f344a760c2bd61c9e7a144d0c
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-03 23:12:44 +0700

    Length of Last Word 2nd solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_string/LengthOfLastWordTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_string/LengthOfLastWordTest.java
@@ -22,8 +22,39 @@ class LengthOfLastWordTest {
         Assertions.assertEquals(6, lengthOfLastWord("luffy is still joyboy"));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertEquals(5, lengthOfLastWord2("Hello World"));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertEquals(4, lengthOfLastWord2("   fly me   to   the moon  "));
+    }
+
+
+    @Test
+    void case06() {
+        Assertions.assertEquals(6, lengthOfLastWord2("luffy is still joyboy"));
+    }
+
     private int lengthOfLastWord(String s) {
         String[] ss = s.split("\\s+");
         return ss[ss.length-1].length();
+    }
+
+    private int lengthOfLastWord2(String s) {
+        int length = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {
+                if (length > 0) {
+                    break;
+                }
+            }
+        }
+
+        return length;
     }
 }
