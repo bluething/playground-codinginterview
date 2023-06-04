6b2ca3f63123a837edc9a51bc03a7381cb92d0af
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-04 21:57:31 +0700

    Problem

---
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_string/ContainsDuplicateTest.java
@@ -0,0 +1,27 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.top_interview_150.array_string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/contains-duplicate/
+class ContainsDuplicateTest {
+
+    @Test
+    void case01() {
+        Assertions.assertTrue(containsDuplicate(new int[]{1,2,3,1}));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertFalse(containsDuplicate(new int[]{1,2,3,4}));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertTrue(containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
+    }
+
+    private boolean containsDuplicate(int[] nums) {
+        return false;
+    }
+}
---
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_string/TwoSumTest.java
@@ -0,0 +1,30 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.top_interview_150.array_string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/two-sum/?envType=study-plan-v2&envId=top-interview-150
+class TwoSumTest {
+
+    @Test
+    void case01() {
+        int[] expected = new int[]{0,1};
+        Assertions.assertArrayEquals(expected, twoSum(new int[]{2,7,11,15}, 9));
+    }
+
+    @Test
+    void case02() {
+        int[] expected = new int[]{1,2};
+        Assertions.assertArrayEquals(expected, twoSum(new int[]{3,2,4}, 6));
+    }
+
+    @Test
+    void case03() {
+        int[] expected = new int[]{0,1};
+        Assertions.assertArrayEquals(expected, twoSum(new int[]{3,3}, 6));
+    }
+
+    private int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
