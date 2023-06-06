594bac095e98ae7d3fccab01c8879af1cb08295d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-06 23:05:52 +0700

    Linked List Cycle solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/linked_list/LinkedListCycleTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/linked_list/LinkedListCycleTest.java
@@ -30,9 +30,71 @@ class LinkedListCycleTest {
         Assertions.assertFalse(hasCycle(one));
     }
 
+    @Test
+    void case04() {
+        ListNode two = new ListNode(2);
+        ListNode one = new ListNode(1, two);
+        Assertions.assertFalse(hasCycle(one));
+    }
+
+    @Test
+    void case05() {
+        ListNode minus4 = new ListNode(-4);
+        ListNode zero = new ListNode(0, minus4);
+        ListNode two = new ListNode(2, zero);
+        minus4.next = two;
+        ListNode three = new ListNode(3, two);
+        Assertions.assertTrue(hasCycle2(three));
+    }
+
+    @Test
+    void case06() {
+        ListNode two = new ListNode(2);
+        ListNode one = new ListNode(1, two);
+        two.next = one;
+        Assertions.assertTrue(hasCycle2(one));
+    }
+
+    @Test
+    void case07() {
+        ListNode one = new ListNode(1);
+        Assertions.assertFalse(hasCycle2(one));
+    }
+
+    @Test
+    void case08() {
+        ListNode two = new ListNode(2);
+        ListNode one = new ListNode(1, two);
+        Assertions.assertFalse(hasCycle2(one));
+    }
+
+    // it's like a race
+    // slower person will be catch up by faster person
     private boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && slow != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
         return false;
     }
+    private boolean hasCycle2(ListNode head) {
+        int mark = Integer.MIN_VALUE;
+        while (head != null) {
+            if (head.val == mark) {
+                return true;
+            }
+            head.val = mark;
+            head = head.next;
+        }
+        return false;
+    }
+
 
     class ListNode {
         int val;
