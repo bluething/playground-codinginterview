edc87ab76925368da5456e13e17bdfdd9d96f3bb
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-07 22:38:44 +0700

    Remove Duplicates from Sorted Array solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_hashing_string/RemoveDuplicatesFromSortedArrayTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_hashing_string/RemoveDuplicatesFromSortedArrayTest.java
@@ -16,7 +16,17 @@ class RemoveDuplicatesFromSortedArrayTest {
         Assertions.assertEquals(5, removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
     }
 
+    // we need a pointer pointing to unique num
+    // when we found element that have value not equal to previous element then increment the pointer (pointing to the next duplicate)
+    // then we assign the value to the element pointed by the pointer
     private int removeDuplicates(int[] nums) {
-        return 0;
+        int uniquePointer = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                uniquePointer++;
+                nums[uniquePointer] = nums[i];
+            }
+        }
+        return uniquePointer+1;
     }
 }
