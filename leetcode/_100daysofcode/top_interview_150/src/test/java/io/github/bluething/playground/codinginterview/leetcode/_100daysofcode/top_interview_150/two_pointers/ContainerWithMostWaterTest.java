7e5e0ab6189890dafbaec1865e791f0d5afca8ba
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-13 23:44:41 +0700

    Container With Most Water solution 1
    5 ms

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/two_pointers/ContainerWithMostWaterTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/two_pointers/ContainerWithMostWaterTest.java
@@ -28,6 +28,16 @@ class ContainerWithMostWaterTest {
 
     private int maxArea(int[] height) {
         int result = Integer.MIN_VALUE;
+        int left = 0;
+        int right = height.length-1;
+        while (left < right) {
+            result = Math.max(result, (right-left) * (Math.min(height[right], height[left])));
+            if (height[right] < height[left]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
 
         return result;
     }
