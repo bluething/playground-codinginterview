92cf1cbc2bbc2e89f08f4cd62c1e839e92184559
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-13 23:36:31 +0700

    problem

---
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/two_pointers/ContainerWithMostWaterTest.java
@@ -0,0 +1,34 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.top_interview_150.two_pointers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
+class ContainerWithMostWaterTest {
+
+    @Test
+    void case01() {
+        Assertions.assertEquals(1, maxArea(new int[]{1, 1}));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertEquals(16, maxArea(new int[]{4,3,2,1,4}));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertEquals(2, maxArea(new int[]{1, 2, 1}));
+    }
+
+    @Test
+    void case04() {
+        Assertions.assertEquals(49, maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
+
+    private int maxArea(int[] height) {
+        int result = Integer.MIN_VALUE;
+
+        return result;
+    }
+}
