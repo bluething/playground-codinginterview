cb867fb95e4911a4715b51b5fa5b1f3413b5e4c9
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-14 22:43:09 +0700

    Longest Substring Without Repeating Characters solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/sliding_window/LongestSubstringWithoutRepeatingCharactersTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/sliding_window/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.t
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
 class LongestSubstringWithoutRepeatingCharactersTest {
 
@@ -36,8 +39,28 @@ class LongestSubstringWithoutRepeatingCharactersTest {
         Assertions.assertEquals(3, lengthOfLongestSubstring("abccabcbb"));
     }
 
+    // using Set as a sliding window
+    // keep add to the Set and calculate maxLength
+    // when find duplicate we remove element from Set start from the left until meet the duplicate char
+    // also increment the start index, so the new index pointing to the next non duplicate char
     private int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
+        int start = 0;
+        int end = 0;
+        Set<Character> windows = new HashSet<>();
+        char current = (char) 0;
+        while (end < s.length()) {
+            current = s.charAt(end);
+            if (windows.add(current)) {
+                maxLength = Math.max(maxLength, windows.size());
+                end++;
+            } else {
+                while (windows.contains(current)) {
+                    windows.remove(s.charAt(start));
+                    start++;
+                }
+            }
+        }
 
         return maxLength;
     }
