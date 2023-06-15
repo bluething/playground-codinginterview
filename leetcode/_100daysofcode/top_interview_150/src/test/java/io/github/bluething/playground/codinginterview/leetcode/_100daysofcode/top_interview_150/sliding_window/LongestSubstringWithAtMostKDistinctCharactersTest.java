1068e7190f8b04fa8431c3e18a76442bf8537133
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-15 23:59:13 +0700

    solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/sliding_window/LongestSubstringWithAtMostKDistinctCharactersTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/sliding_window/LongestSubstringWithAtMostKDistinctCharactersTest.java
@@ -4,6 +4,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.t
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/description/
 // https://www.lintcode.com/problem/386/
 class LongestSubstringWithAtMostKDistinctCharactersTest {
@@ -25,6 +28,26 @@ class LongestSubstringWithAtMostKDistinctCharactersTest {
 
     private int lengthOfLongestSubstringKDistinct(String s, int k) {
         int longest = 0;
+        Map<Character, Integer> freqMap = new HashMap<>();
+        int leftIdx = 0;
+        for (int i = 0; i < s.length(); i++) {
+            freqMap.put(s.charAt(i), freqMap.getOrDefault(s.charAt(i), 0) + 1);
+
+            // maintain the window <= k
+            // when the window size > k then we need to shrink it into k again
+            // we delete the element in map when the freq is 0
+            // the leftIdx will point to the char that need to delete
+            // when the window size back to k, the leftIdx will point to the 2nd unique char
+            while (freqMap.size() > k) {
+                freqMap.put(s.charAt(leftIdx), freqMap.get(s.charAt(leftIdx))-1);
+                if (freqMap.get(s.charAt(leftIdx)) == 0) {
+                    freqMap.remove(s.charAt(leftIdx));
+                }
+                leftIdx++;
+            }
+
+            longest = Math.max(longest, i - leftIdx + 1);
+        }
         return longest;
     }
 }
--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/sliding_window/LongestSubstringWithAtMostTwoDistinctCharactersTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/sliding_window/LongestSubstringWithAtMostTwoDistinctCharactersTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.t
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/description/
 // https://aaronice.gitbook.io/lintcode/two_pointers/longest-substring-with-at-most-two-distinct-characters
 class LongestSubstringWithAtMostTwoDistinctCharactersTest {
@@ -17,9 +20,29 @@ class LongestSubstringWithAtMostTwoDistinctCharactersTest {
         Assertions.assertEquals(5, lengthOfLongestSubstringTwoDistinct("ccaabbb"));
     }
 
+    // using map as a sliding window
     private int lengthOfLongestSubstringTwoDistinct(String s) {
         int longest = 0;
+        Map<Character, Integer> freqMap = new HashMap<>();
+        int leftIdx = 0;
+        for (int i = 0; i < s.length(); i++) {
+            freqMap.put(s.charAt(i), freqMap.getOrDefault(s.charAt(i), 0) + 1);
 
+            // maintain the window <= 2
+            // when the window size > 2 then we need to shrink it into 2 again
+            // we delete the element in map when the freq is 0
+            // the leftIdx will point to the char that need to delete
+            // when the window size back to 2, the leftIdx will point to the 2nd unique char
+            while (freqMap.size() > 2) {
+                freqMap.put(s.charAt(leftIdx), freqMap.get(s.charAt(leftIdx))-1);
+                if (freqMap.get(s.charAt(leftIdx)) == 0) {
+                    freqMap.remove(s.charAt(leftIdx));
+                }
+                leftIdx++;
+            }
+
+            longest = Math.max(longest, i - leftIdx + 1);
+        }
         return longest;
     }
 }
