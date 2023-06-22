f5a5c22c408524d4995989be52a0f78b06daa2c8
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-22 23:51:55 +0700

    Delete Node in a Linked List solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/linked_list/DeleteNodeInALinkedListTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/linked_list/DeleteNodeInALinkedListTest.java
@@ -2,8 +2,15 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.t
 
 // https://leetcode.com/problems/delete-node-in-a-linked-list/description/
 class DeleteNodeInALinkedListTest {
-    private void deleteNode(ListNode node) {
 
+    // You are given the node to be deleted node. So we have node to be delete
+    // the key is "It is guaranteed that the node to be deleted is not a tail node in the list."
+    // it means that the node given is between 1st node and one node before tail
+    // actually we remove the next node after given node
+    // but before that we copy the value then change next pointer
+    private void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 
     class ListNode {
