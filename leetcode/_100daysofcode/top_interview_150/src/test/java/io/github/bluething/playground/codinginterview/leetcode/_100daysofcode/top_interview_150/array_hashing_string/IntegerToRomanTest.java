61e73cb934f48b06345e83bd4968f494e335fe50
Habib Ali Machpud <habib.machpud@gmail.com>
2023-07-02 14:54:09 +0700

    Integer to Roman 2nd solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_hashing_string/IntegerToRomanTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_hashing_string/IntegerToRomanTest.java
@@ -21,6 +21,21 @@ class IntegerToRomanTest {
         Assertions.assertEquals("MCMXCIV", intToRoman(1994));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertEquals("III", intToRoman2(3));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertEquals("LVIII", intToRoman2(58));
+    }
+
+    @Test
+    void case06() {
+        Assertions.assertEquals("MCMXCIV", intToRoman2(1994));
+    }
+
     // just subtract the num until 0
     // the key is we need to have a dictionary (int-roman) that order desc
     // here I use 2 array
@@ -43,5 +58,23 @@ class IntegerToRomanTest {
         }
 
         return sb.toString();
+    }
+
+    // we have constraint 1 <= num <= 3999
+    //  so there are shortcut to solve this problem
+    // what we do is we only need to find roman for thousands, hundreds, dozens and units part
+    //  just use division and modulo operator
+    // the key is we need to prepare mapping from to roman for all cases (not only base case)
+    //  thousands => 1000 -> 3000
+    //  hundreds => 100, 200, 300,.. -> 900
+    //  dozens => 10, 20, 30,... -> 90
+    //  units => 1, 2, 3.. => 9
+    private String intToRoman2(int num) {
+        String M[] = {"", "M", "MM", "MMM"};
+        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
     }
 }
