8c2113d85df6300fd96140509425cbdfa99b646e
Habib Ali Machpud <habib.machpud@gmail.com>
2023-07-02 14:31:10 +0700

    Integer to Roman problem

---
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/array_hashing_string/IntegerToRomanTest.java
@@ -0,0 +1,27 @@
+package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.top_interview_150.array_hashing_string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/integer-to-roman/description/
+class IntegerToRomanTest {
+
+    @Test
+    void case01() {
+        Assertions.assertEquals("III", intToRoman(3));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertEquals("LVIII", intToRoman(58));
+    }
+
+    @Test
+    void case03() {
+        Assertions.assertEquals("MCMXCIV", intToRoman(1994));
+    }
+
+    private String intToRoman(int num) {
+        return "";
+    }
+}
