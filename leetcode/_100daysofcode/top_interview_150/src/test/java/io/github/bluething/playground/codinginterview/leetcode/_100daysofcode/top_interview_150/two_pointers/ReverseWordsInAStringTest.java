7ebdecf0f5a2a3f8eefeb60defd4f1e2916d31dd
Habib Ali Machpud <habib.machpud@gmail.com>
2023-07-03 23:51:15 +0700

    Reverse Words in a String 2nd solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/two_pointers/ReverseWordsInAStringTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/two_pointers/ReverseWordsInAStringTest.java
@@ -21,6 +21,21 @@ class ReverseWordsInAStringTest {
         Assertions.assertEquals("example good a", reverseWords("a good   example"));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertEquals("blue is sky the", reverseWords2("the sky is blue"));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertEquals("world hello", reverseWords2("  hello world  "));
+    }
+
+    @Test
+    void case06() {
+        Assertions.assertEquals("example good a", reverseWords2("a good   example"));
+    }
+
     // the key is we need to split by space
     // then save to an array
     // then iterate from last element to construct the result
@@ -31,5 +46,35 @@ class ReverseWordsInAStringTest {
             sb.append(ss[i]).append(" ");
         }
         return sb.toString().trim();
+    }
+
+    // just convert to an array then iterate from last element
+    // what we need to do is identify the word sequence
+    // from last i find 1st space value
+    //  this index will be a 1st char in the word
+    private String reverseWords2(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder("");
+        for (int i = chars.length-1; i>=0; i--) {
+            if (chars[i] == ' ') {
+                continue;
+            }
+
+            int j=i;
+            while (j >= 0 && chars[j] != ' ') {
+                j--;
+            }
+            // need to +1 because in the code above the j index will point to the space value
+            for (int k = j+1; k <= i; k++) {
+                sb.append(chars[k]);
+            }
+            sb.append(" ");
+
+            i = j;
+        }
+
+        // remove extra space
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
     }
 }
