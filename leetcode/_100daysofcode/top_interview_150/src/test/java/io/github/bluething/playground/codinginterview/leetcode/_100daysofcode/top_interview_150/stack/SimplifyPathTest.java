f8709ee376eee12d29b54821cb2762308fec9f27
Habib Ali Machpud <habib.machpud@gmail.com>
2023-07-10 00:05:18 +0700

    Simplify Path solution

--- leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/stack/SimplifyPathTest.java
+++ leetcode/_100daysofcode/top_interview_150/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/top_interview_150/stack/SimplifyPathTest.java
@@ -3,12 +3,16 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.t
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
 // https://leetcode.com/problems/simplify-path/?envType=study-plan-v2&envId=top-interview-150
 class SimplifyPathTest {
 
     @Test
     void case01() {
-        Assertions.assertEquals("/home/", simplifyPath("/home/"));
+        Assertions.assertEquals("/home", simplifyPath("/home/"));
     }
 
     @Test
@@ -21,7 +25,56 @@ class SimplifyPathTest {
         Assertions.assertEquals("/home/foo", simplifyPath("/home//foo/"));
     }
 
+    @Test
+    void case04() {
+        Assertions.assertEquals("/home", simplifyPath2("/home/"));
+    }
+
+    @Test
+    void case05() {
+        Assertions.assertEquals("/", simplifyPath2("/../"));
+    }
+
+    @Test
+    void case06() {
+        Assertions.assertEquals("/home/foo", simplifyPath2("/home//foo/"));
+    }
+
+    // split the path by /
+    //  we only need to take care . or .. or empty (double slash)
     private String simplifyPath(String path) {
-        return "";
+        Stack<String> stack = new Stack<>();
+        for (String dir : path.split("/")) {
+            if (dir.equals(".") || dir.isEmpty()) {
+                continue;
+            }
+
+            if (dir.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(dir);
+            }
+        }
+        return "/" + String.join("/", stack);
+    }
+
+    // use ArrayDeque if we want to add/remove items to both ends
+    // use LinkedLists if we're doing lots of add/remove in the middle.
+    private String simplifyPath2(String path) {
+        Deque<String> deque = new ArrayDeque<>();
+        for (String dir : path.split("/")) {
+            if (dir.equals(".") || dir.isEmpty()) {
+                continue;
+            }
+
+            if (dir.equals("..")) {
+                deque.pollLast();
+            } else {
+                deque.add(dir);
+            }
+        }
+        return "/" + String.join("/", deque);
     }
 }
