34f3da1fcd110fd6a47dd20d89021de2b9a2803c
Habib Ali Machpud <habib.machpud@gmail.com>
2023-09-16 23:10:52 +0700

    Roman to Integer solution

--- leetcode/_100daysofcode/part_four/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partfour/array_hashing_string/RomanToIntegerTest.java
+++ leetcode/_100daysofcode/part_four/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partfour/array_hashing_string/RomanToIntegerTest.java
@@ -3,6 +3,10 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.plugins.jpeg.JPEGImageReadParam;
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode.com/problems/roman-to-integer/?envType=study-plan-v2&envId=top-interview-150
 class RomanToIntegerTest {
 
@@ -22,6 +26,28 @@ class RomanToIntegerTest {
     }
 
     private int romanToInt(String s) {
-        return 0;
+        Map<Character, Integer> romans = new HashMap<>();
+        romans.put('I', 1);
+        romans.put('V', 5);
+        romans.put('X', 10);
+        romans.put('L', 50);
+        romans.put('C', 100);
+        romans.put('D', 500);
+        romans.put('M', 1000);
+
+        int romanInt = 0;
+        int current = 0;
+        int prev = 0;
+        for (char roman : s.toCharArray()) {
+            current = romans.get(roman);
+            romanInt += current;
+            if (current > prev) {
+                romanInt -= prev * 2;
+            }
+
+            prev = current;
+        }
+
+        return romanInt;
     }
 }
