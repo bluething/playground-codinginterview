644968940f870a6be1262f662454d7fc78ab34d2
Habib Ali Machpud <habib.machpud@gmail.com>
2023-09-17 18:19:22 +0700

    Merge Strings Alternately solution

--- leetcode/_100daysofcode/part_four/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partfour/two_pointers/MergeStringsAlternatelyTest.java
+++ leetcode/_100daysofcode/part_four/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partfour/two_pointers/MergeStringsAlternatelyTest.java
@@ -20,6 +20,22 @@ class MergeStringsAlternatelyTest {
     }
 
     private String mergeAlternately(String word1, String word2) {
-        return "";
+        int i = 0;
+        int j = 0;
+        StringBuilder result = new StringBuilder();
+        while (i < word1.length() && j < word2.length()) {
+            result.append(word1.charAt(i));
+            result.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+        if (i < word1.length()) {
+            result.append(word1.substring(i));
+        }
+        if (j < word2.length()) {
+            result.append(word2.substring(j));
+        }
+
+        return result.toString();
     }
 }
