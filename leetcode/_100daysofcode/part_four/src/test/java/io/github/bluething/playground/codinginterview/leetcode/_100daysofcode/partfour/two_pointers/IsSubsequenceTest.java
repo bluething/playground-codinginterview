ec94653b1f6d7b7887768b5dca01309956307f30
Habib Ali Machpud <habib.machpud@gmail.com>
2023-09-17 16:19:38 +0700

    Is Subsequence solution

--- leetcode/_100daysofcode/part_four/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partfour/two_pointers/IsSubsequenceTest.java
+++ leetcode/_100daysofcode/part_four/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/partfour/two_pointers/IsSubsequenceTest.java
@@ -20,7 +20,15 @@ class IsSubsequenceTest {
     }
 
     private boolean isSubsequence(String s, String t) {
-        return true;
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
     }
 
 }
