21a1dc2357f4942dcd76e0f3eec21c8fceb19c8b
Habib Ali Machpud <habib.machpud@gmail.com>
2023-11-05 22:15:12 +0700

    GreetingCard solution

--- loop-bootcamp/loop_00-set/src/main/java/io/github/bluething/playground/codinginterview/loop/GreetingCard.java
+++ loop-bootcamp/loop_00-set/src/main/java/io/github/bluething/playground/codinginterview/loop/GreetingCard.java
@@ -3,6 +3,7 @@ package io.github.bluething.playground.codinginterview.loop;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class GreetingCard {
     public static void main(String[] args) throws IOException {
@@ -14,14 +15,57 @@ public class GreetingCard {
             inputParts = bufferedReader.readLine().split(" ");
             points[i] = new Coordinate(Integer.parseInt(inputParts[0]), Integer.parseInt(inputParts[1]));
         }
+        List<Coordinate> pythagoreanCoordinates = generatePythagoreanCoordinate();
+        Set<Coordinate> visitedPoints = new HashSet<>();
+        int numOfPair = 0;
+        for (Coordinate coordinate : points) {
+            for (Coordinate pythagoreanCoordinate : pythagoreanCoordinates) {
+                if (visitedPoints.contains(new Coordinate(coordinate.x + pythagoreanCoordinate.x, coordinate.y + pythagoreanCoordinate.y))) {
+                    numOfPair++;
+                }
+            }
+            visitedPoints.add(coordinate);
+        }
+
+        System.out.print(numOfPair--);
+    }
+
+    private static List<Coordinate> generatePythagoreanCoordinate() {
+        List<Coordinate> pythagoreanCoordinates = new ArrayList<>();
+        pythagoreanCoordinates.add(new Coordinate(2018, 0));
+        pythagoreanCoordinates.add(new Coordinate(-2018, 0));
+        pythagoreanCoordinates.add(new Coordinate(0, 2018));
+        pythagoreanCoordinates.add(new Coordinate(0, -2018));
+        pythagoreanCoordinates.add(new Coordinate(1680, 1118));
+        pythagoreanCoordinates.add(new Coordinate(1680, -1118));
+        pythagoreanCoordinates.add(new Coordinate(-1680, 1118));
+        pythagoreanCoordinates.add(new Coordinate(-1680, -1118));
+        pythagoreanCoordinates.add(new Coordinate(1118, 1680));
+        pythagoreanCoordinates.add(new Coordinate(1118, -1680));
+        pythagoreanCoordinates.add(new Coordinate(-1118, 1680));
+        pythagoreanCoordinates.add(new Coordinate(-1118, -1680));
+        return pythagoreanCoordinates;
     }
 }
 
 class Coordinate {
-    int x;
-    int y;
-    public Coordinate(int x, int y) {
+    long x;
+    long y;
+    public Coordinate(long x, long y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
