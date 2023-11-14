40107a62f7a3f79443c6645c1c8bb79f0a8b1b2d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-11-15 00:23:21 +0700

    Sim 1st solution
    Hit TLE

---
+++ loop-bootcamp/loop_02_bit_stack_queue/src/main/java/io/github/bluething/playground/codinginterview/loop/Sim.java
@@ -0,0 +1,39 @@
+package io.github.bluething.playground.codinginterview.loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Sim {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int numOfTestCase = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < numOfTestCase; i++) {
+            String input = bufferedReader.readLine();
+            LinkedList<Character> stack = new LinkedList<>();
+            ListIterator<Character> cursor = stack.listIterator();
+
+            for (char c : input.toCharArray()) {
+                if ('<' == c) {
+                    if (cursor.hasPrevious()) {
+                        cursor.previous();
+                        cursor.remove();
+                    }
+                } else if ('[' == c) {
+                    cursor = stack.listIterator();
+                } else if (']' == c) {
+                    cursor = stack.listIterator(stack.size());
+                } else {
+                    cursor.add(c);
+                }
+            }
+
+            for (char c : stack) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
