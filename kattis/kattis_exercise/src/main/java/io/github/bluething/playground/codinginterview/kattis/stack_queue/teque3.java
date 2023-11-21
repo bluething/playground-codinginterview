1998211984547b76ec16b19895b94dad85b54a32
Habib Ali Machpud <habib.machpud@gmail.com>
2023-11-21 23:07:07 +0700

    Teque 3rd solution using Kattio class

--- kattis/kattis_exercise/src/main/java/io/github/bluething/playground/codinginterview/kattis/stack_queue/teque3.java
+++ kattis/kattis_exercise/src/main/java/io/github/bluething/playground/codinginterview/kattis/stack_queue/teque3.java
@@ -1,16 +1,13 @@
 package io.github.bluething.playground.codinginterview.kattis.stack_queue;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import io.github.bluething.playground.codinginterview.kattis.Kattio;
+
 import java.util.Hashtable;
 
 public class teque3 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int numOfOps = Integer.parseInt(bufferedReader.readLine());
-        String[] ops;
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        int numOfOps = io.getInt();
         String command;
         Hashtable<Integer, Integer> queueFront = new Hashtable<>();
         int queueFrontStart = -1;
@@ -18,17 +15,16 @@ public class teque3 {
         Hashtable<Integer, Integer> queueBack = new Hashtable<>();
         int queueBackStart = -1;
         int queueBackEnd = 0;
-        PrintWriter writer = new PrintWriter(System.out);
+
         for (int i = 0; i < numOfOps; i++) {
-            ops = bufferedReader.readLine().split(" ");
-            command = ops[0];
-            int x = Integer.parseInt(ops[1]);
+            command = io.getWord();
+            int x = io.getInt();
 
             if ("get".equals(command)) {
                 if (x >= queueFront.size()) {
-                    writer.println(queueBack.get(x - queueFront.size() + queueBackStart + 1));
+                    io.println(queueBack.get(x - queueFront.size() + queueBackStart + 1));
                 } else {
-                    writer.println(queueFront.get(x + queueFrontStart + 1));
+                    io.println(queueFront.get(x + queueFrontStart + 1));
                 }
 
                 continue;
@@ -56,6 +52,6 @@ public class teque3 {
             }
         }
 
-        writer.close();
+        io.close();
     }
 }
