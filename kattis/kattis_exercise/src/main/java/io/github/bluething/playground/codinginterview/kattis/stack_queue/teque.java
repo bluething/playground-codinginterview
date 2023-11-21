e35674c2714ce14018fac240cd537901b82433e7
Habib Ali Machpud <habib.machpud@gmail.com>
2023-11-21 23:10:28 +0700

    Teque 1st solution
    Even I use Kattio, still hit TLE

--- kattis/kattis_exercise/src/main/java/io/github/bluething/playground/codinginterview/kattis/stack_queue/teque.java
+++ kattis/kattis_exercise/src/main/java/io/github/bluething/playground/codinginterview/kattis/stack_queue/teque.java
@@ -1,21 +1,18 @@
 package io.github.bluething.playground.codinginterview.kattis.stack_queue;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import io.github.bluething.playground.codinginterview.kattis.Kattio;
+
 import java.util.LinkedList;
 
 public class teque {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int numOfOps = Integer.parseInt(bufferedReader.readLine());
-        String[] ops;
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        int numOfOps = io.getInt();
         String command;
         LinkedList<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numOfOps; i++) {
-            ops = bufferedReader.readLine().split(" ");
-            command = ops[0];
-            int x = Integer.parseInt(ops[1]);
+            command = io.getWord();
+            int x = io.getInt();
 
             if ("push_back".equals(command)) {
                 queue.addLast(x);
@@ -24,8 +21,10 @@ public class teque {
             } else if ("push_middle".equals(command)) {
                 queue.add((queue.size()+1)/2, x);
             } else {
-                System.out.println(queue.get(x));
+                io.println(queue.get(x));
             }
         }
+
+        io.close();
     }
 }
