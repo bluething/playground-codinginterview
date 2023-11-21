17f1af7e913b86b29bb6152564dc8603f3a4b15d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-11-21 22:52:10 +0700

    Teque 2nd solution using kattio
    Even if I change the io, TLE still happen

--- kattis/kattis_exercise/src/main/java/io/github/bluething/playground/codinginterview/kattis/stack_queue/teque2.java
+++ kattis/kattis_exercise/src/main/java/io/github/bluething/playground/codinginterview/kattis/stack_queue/teque2.java
@@ -1,22 +1,20 @@
 package io.github.bluething.playground.codinginterview.kattis.stack_queue;
 
-import java.io.BufferedReader;
+import io.github.bluething.playground.codinginterview.kattis.Kattio;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 
 public class teque2 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int numOfOps = Integer.parseInt(bufferedReader.readLine());
-        String[] ops;
+        Kattio io = new Kattio(System.in, System.out);
+        int numOfOps = io.getInt();
         String command;
         LinkedList<Integer> queueFront = new LinkedList<>();
         LinkedList<Integer> queueBack = new LinkedList<>();
         for (int i = 0; i < numOfOps; i++) {
-            ops = bufferedReader.readLine().split(" ");
-            command = ops[0];
-            int x = Integer.parseInt(ops[1]);
+            command = io.getWord();
+            int x = io.getInt();
 
             if ("push_back".equals(command)) {
                 queueBack.offerLast(x);
@@ -29,12 +27,14 @@ public class teque2 {
                 balancing(queueFront, queueBack);
             } else {
                 if (queueFront.size() > x) {
-                    System.out.println(queueFront.get(x));
+                    io.println(queueFront.get(x));
                 } else {
-                    System.out.println(queueBack.get(x - queueFront.size()));
+                    io.println(queueBack.get(x - queueFront.size()));
                 }
             }
         }
+
+        io.close();
     }
 
     private static void balancing(LinkedList<Integer> queueFront, LinkedList<Integer> queueBack) {
