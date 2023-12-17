743492ddc8099e6a65fd7410d3143fcc827d9fe7
Habib Ali Machpud <habib.machpud@gmail.com>
2023-12-17 14:30:46 +0700

    Max Area of Islands solution
    Using union find approach

--- leetcode/_100daysofcode/part_five/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/part_five/arrays_hashing_string/MaxAreaOfIslandTest.java
+++ leetcode/_100daysofcode/part_five/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/part_five/arrays_hashing_string/MaxAreaOfIslandTest.java
@@ -3,6 +3,10 @@ package io.github.bluething.playground.codinginterview.leetcode._100daysofcode.p
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode.com/problems/max-area-of-island/
 class MaxAreaOfIslandTest {
     @Test
@@ -22,24 +26,72 @@ class MaxAreaOfIslandTest {
     }
 
     private int maxAreaOfIsland(int[][] grid) {
-        return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        UnionFind unionFind = new UnionFind(row * col);
+        Map<Integer, Integer> sizeOfIndex = new HashMap<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    int workingIndex = unionFind.getIndex(i, j, col);
+                    if (i+1 < row && grid[i+1][j] == 1) {
+                        unionFind.connect(workingIndex, unionFind.getIndex(i+1, j, col));
+                    }
+                    if (j+1 < col && grid[i][j+1] == 1) {
+                        unionFind.connect(workingIndex, unionFind.getIndex(i, j+1, col));
+                    }
+
+                    sizeOfIndex.putIfAbsent(workingIndex, unionFind.sizeOfSet(workingIndex));
+                }
+            }
+        }
+
+        return sizeOfIndex.isEmpty() ? 0 : Collections.max(sizeOfIndex.values());
     }
 
     static class UnionFind {
         private final int[] parent;
         private final int[] sizes;
-        private final boolean[] blacklisted;
-        private int numOfSet;
 
         public UnionFind(int N) {
             this.parent = new int[N];
             this.sizes = new int[N];
-            this.blacklisted = new boolean[N];
-            numOfSet = N;
             for (int i = 0; i < N; i++) {
                 parent[i] = i;
                 sizes[i] = 1;
             }
+        }
+
+        private int findSet(int i) {
+            if (parent[i] == i) {
+                return i;
+            }
+
+            return parent[i] = findSet(parent[i]);
+        }
+
+        public void connect(int i, int j) {
+            int parentI = findSet(i);
+            int parentJ = findSet(j);
+            if (parentI == parentJ) {
+                return;
+            }
+
+            if (sizes[parentI] < sizes[parentJ]) {
+                parent[parentI] = parentJ;
+                sizes[parentJ] += sizes[parentI];
+            } else {
+                parent[parentJ] = parentI;
+                sizes[parentI] += sizes[parentJ];
+            }
+        }
+
+        public int sizeOfSet(int i) {
+            return sizes[findSet(i)];
+        }
+
+        public int getIndex(int row, int col, int numOfColumn) {
+            return row * numOfColumn + col;
         }
     }
 }
