6407326e2ee7d09c24681ae46f204eb00f8cffaa
Habib Ali Machpud <habib.machpud@gmail.com>
2023-12-17 13:59:57 +0700

    Count Sub Islands solution

--- leetcode/_100daysofcode/part_five/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/part_five/arrays_hashing_string/CountSubIslandsTest.java
+++ leetcode/_100daysofcode/part_five/src/test/java/io/github/bluething/playground/codinginterview/leetcode/_100daysofcode/part_five/arrays_hashing_string/CountSubIslandsTest.java
@@ -18,7 +18,26 @@ class CountSubIslandsTest {
     }
 
     private int countSubIslands(int[][] grid1, int[][] grid2) {
-        return 0;
+        int row = grid1.length;
+        int col = grid1[0].length;
+        UnionFind unionFind = new UnionFind(row * col);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid2[i][j] == 0 || grid1[i][j] == 0) {
+                    unionFind.blackListComponent(unionFind.index(i, j, col));
+                }
+                if (grid2[i][j] == 1) {
+                    if (i+1 < row && grid2[i+1][j] == 1) {
+                        unionFind.connect(unionFind.index(i, j, col), unionFind.index(i+1, j, col));
+                    }
+                    if (j+1 < col && grid2[i][j+1] == 1) {
+                        unionFind.connect(unionFind.index(i, j, col), unionFind.index(i, j+1, col));
+                    }
+                }
+            }
+        }
+
+        return unionFind.numOfSet;
     }
 
     static class UnionFind {
@@ -36,6 +55,50 @@ class CountSubIslandsTest {
                 parent[i] = i;
                 sizes[i] = 1;
             }
+        }
+
+        private int findSet(int i) {
+            if (parent[i] == i) {
+                return i;
+            }
+
+            return parent[i] = findSet(parent[i]);
+        }
+        private void connectTo(int from, int to) {
+            sizes[to] += sizes[from];
+            parent[from] = to;
+            if (!blacklisted[to] || !blacklisted[from]) {
+                numOfSet--;
+            }
+            if (blacklisted[from]) {
+                blacklisted[to] = true;
+            }
+        }
+
+        public void connect(int i, int j) {
+            int parentI = findSet(i);
+            int parentJ = findSet(j);
+            if (parentI == parentJ) {
+                return;
+            }
+
+            if (sizes[parentI] < sizes[parentJ]) {
+                connectTo(parentI, parentJ);
+            } else {
+                connectTo(parentJ, parentI);
+            }
+        }
+
+        public void blackListComponent(int i) {
+            int parentI = findSet(i);
+            if (!blacklisted[parentI]) {
+                numOfSet--;
+            }
+            blacklisted[parentI] = true;
+        }
+
+        public int index(int row, int col, int numOfColumn) {
+            return row * numOfColumn + col;
         }
     }
 }
