ca3704a300b4ff32dd1b0faf6bcff3c2a20297ea
Habib Ali Machpud <habib.machpud@gmail.com>
2024-04-18 00:45:19 +0700

    New module

---
+++ hackerrank/_1monthpreparationkit/1-week_one/src/main/java/io/github/bluething/playground/codinginterview/hackerrank/_1monthpreparationkit/Main.java
@@ -0,0 +1,7 @@
+package io.github.bluething.playground.codinginterview.hackerrank._1monthpreparationkit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
