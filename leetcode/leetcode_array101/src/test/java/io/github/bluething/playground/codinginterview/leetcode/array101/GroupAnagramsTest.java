99d1aa9fa203649e17675ef98e8ea6270b2313f1
Habib Ali Machpud <habib.machpud@gmail.com>
2024-04-23 13:35:54 +0700

    Group Anagrams solution

---
+++ leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/GroupAnagramsTest.java
@@ -0,0 +1,49 @@
+package io.github.bluething.playground.codinginterview.leetcode.array101;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class GroupAnagramsTest {
+    @Test
+    void case01() {
+        List<List<String>> expected = Arrays.asList(Arrays.asList("eat","tea","ate"),
+                Arrays.asList("bat"),
+                Arrays.asList("tan", "nat"));
+        Assertions.assertEquals(expected, groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+    }
+
+    @Test
+    void case02() {
+        List<List<String>> expected = Arrays.asList(Arrays.asList(""));
+        Assertions.assertEquals(expected, groupAnagrams(new String[]{""}));
+    }
+
+    @Test
+    void case03() {
+        List<List<String>> expected = Arrays.asList(Arrays.asList("a"));
+        Assertions.assertEquals(expected, groupAnagrams(new String[]{"a"}));
+    }
+
+    private List<List<String>> groupAnagrams(String[] strs) {
+        int[] freqChar = new int[26];
+        String freqS = "";
+        Map<String, List<String>> anagrams = new HashMap<>();
+        for (String str : strs) {
+            freqChar = new int[26];
+            for (char c : str.toCharArray()) {
+                freqChar[c - 'a']++;
+            }
+            freqS = Arrays.toString(freqChar);
+
+            if (anagrams.get(freqS) == null) {
+                anagrams.put(freqS, new ArrayList<>());
+            }
+
+            anagrams.get(freqS).add(str);
+        }
+
+        return new ArrayList<>(anagrams.values());
+    }
+}
