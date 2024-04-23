32f3bdf38c5d26467805f855c17a4e4b7c83e6f5
Habib Ali Machpud <habib.machpud@gmail.com>
2024-04-23 14:25:55 +0700

    Product of Array Except Self solution

---
+++ leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,32 @@
+package io.github.bluething.playground.codinginterview.leetcode.array101;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ProductOfArrayExceptSelfTest {
+    @Test
+    void case01() {
+        Assertions.assertArrayEquals(new int[]{24,12,8,6}, productExceptSelf(new int[]{1,2,3,4}));
+    }
+
+    @Test
+    void case02() {
+        Assertions.assertArrayEquals(new int[]{0,0,9,0,0}, productExceptSelf(new int[]{-1,1,0,-3,3}));
+    }
+
+    private int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int prefix = 1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int suffix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return result;
+    }
+}
