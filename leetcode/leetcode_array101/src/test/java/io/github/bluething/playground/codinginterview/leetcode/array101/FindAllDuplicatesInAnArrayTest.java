ddbc22d3a8cf392a13949c0b9ed840ba072ce353
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-07 23:42:44 +0700

    Solution FindAllDuplicatesInAnArray

--- leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/FindAllDuplicatesInAnArrayTest.java
+++ leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/FindAllDuplicatesInAnArrayTest.java
@@ -3,6 +3,7 @@ package io.github.bluething.playground.codinginterview.leetcode.array101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,6 +22,15 @@ class FindAllDuplicatesInAnArrayTest {
     }
 
     private List<Integer> findDuplicates(int[] nums) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]);
+            if (nums[idx-1] < 0) {
+                result.add(idx);
+            } else {
+                nums[idx-1] *= -1;
+            }
+        }
+        return result;
     }
 }
