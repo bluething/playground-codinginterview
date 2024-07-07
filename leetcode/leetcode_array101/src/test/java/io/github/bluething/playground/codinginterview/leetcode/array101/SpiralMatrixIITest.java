d2259ac0ba5c9fd950a88405c62f00f97965ddbc
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-07 21:27:25 +0700

    Solution SpiralMatrixII

--- leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/SpiralMatrixIITest.java
+++ leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/SpiralMatrixIITest.java
@@ -14,6 +14,36 @@ class SpiralMatrixIITest {
     }
 
     private int[][] generateMatrix(int n) {
-        return new int[][]{};
+        int top = 0;
+        int bottom = n-1;
+        int left = 0;
+        int right = n-1;
+        int[][] result = new int[n][n];
+        int counter = 1;
+        while (counter <= n * n) {
+            for (int i = left; i <= right; i++) {
+                result[top][i] = counter++;
+            }
+            top++;
+            if (left > right || top > bottom) {
+                break;
+            }
+            for (int i = top; i <= bottom; i++) {
+                result[i][right] = counter++;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                result[bottom][i] = counter++;
+            }
+            bottom--;
+            if (left > right || top > bottom) {
+                break;
+            }
+            for (int i = bottom; i >= top; i--) {
+                result[i][left] = counter++;
+            }
+            left++;
+        }
+        return result;
     }
 }
