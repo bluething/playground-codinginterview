ceea81df90e31c2c20a70745bdc5479fddbbc7a2
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-07 23:18:09 +0700

    Soution NextPermutation

--- leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/NextPermutationTest.java
+++ leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/NextPermutationTest.java
@@ -24,6 +24,34 @@ class NextPermutationTest {
     }
 
     private void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        // find 1st decreasing element
+        while (i >= 0 && nums[i] >= nums[i+1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            // swap 1st decreasing (i) element with 1st ascending element (compare to nums[i] from the right
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
 
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
