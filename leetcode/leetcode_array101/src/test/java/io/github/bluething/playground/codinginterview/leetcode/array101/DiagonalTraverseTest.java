7f9aa7842426dc955c0cb238ca66fd55d5f2f9a8
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-07 16:27:38 +0700

    Solution DiagonalTraverse

--- leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/DiagonalTraverseTest.java
+++ leetcode/leetcode_array101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/array101/DiagonalTraverseTest.java
@@ -10,10 +10,46 @@ class DiagonalTraverseTest {
     }
     @Test
     void case02() {
-        Assertions.assertArrayEquals(new int[]{}, findDiagonalOrder(new int[][]{{1,2},{3,4}}));
+        Assertions.assertArrayEquals(new int[]{1,2,3,4}, findDiagonalOrder(new int[][]{{1,2},{3,4}}));
     }
 
     private int[] findDiagonalOrder(int[][] mat) {
-        return new int[]{};
+        int row = mat.length;
+        int col = mat[0].length;
+        if (row == 0 || col == 0) {
+            return new int[]{};
+        }
+
+        // x: vertical, y: horizontal
+        int x = 0, y = 0;
+        int[] result = new int[row * col];
+        int idx = 0;
+        while (idx < result.length) {
+            result[idx++] = mat[y][x];
+            boolean isEven = (x+y) % 2 == 0;
+            // even: up right, odd: down left
+            if (isEven) {
+                if (x < col - 1 && y > 0) {
+                    x++;
+                    y--;
+                    // hit edge (y=0)
+                } else if (x < col - 1) {
+                    x++;
+                    // outside egde (y<0)
+                } else {
+                    y++;
+                }
+            } else {
+                if (y < row - 1 && x > 0) {
+                    y++;
+                    x--;
+                } else if (y < row - 1) {
+                    y++;
+                } else {
+                    x++;
+                }
+            }
+        }
+        return result;
     }
 }
