aeff186737816bc20582aab2f6d1d4231eead77b
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-18 14:11:50 +0700

    solution

--- leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/_3SumClosestTest.java
+++ leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/_3SumClosestTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode.twopointers101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class _3SumClosestTest {
     @Test
     void case01() {
@@ -14,6 +16,30 @@ public class _3SumClosestTest {
     }
 
     private int threeSumClosest(int[] nums, int target) {
-        return 0;
+        Arrays.sort(nums);
+        int gap = Integer.MAX_VALUE;
+        int ans = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    return sum;
+                }
+                if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+
+                int curGap = Math.abs(sum - target);
+                if (curGap < gap) {
+                    gap = curGap;
+                    ans = sum;
+                }
+            }
+        }
+        return ans;
     }
 }
