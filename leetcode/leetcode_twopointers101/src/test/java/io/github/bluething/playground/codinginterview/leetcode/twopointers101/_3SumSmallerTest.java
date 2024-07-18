16be9f31783e6d513352641c125be67f6b68c5d7
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-18 14:34:35 +0700

    solution

--- leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/_3SumSmallerTest.java
+++ leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/_3SumSmallerTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode.twopointers101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 class _3SumSmallerTest {
     @Test
     void case01(){
@@ -10,9 +12,24 @@ class _3SumSmallerTest {
     }
     @Test
     void case02(){
-        Assertions.assertEquals(3, threeSumSmaller(new int[]{-2,0,1,3}, 2));
+        Assertions.assertEquals(2, threeSumSmaller(new int[]{-2,0,1,3}, 2));
     }
     private int threeSumSmaller(int[] nums, int target) {
-        // Write your code here
+        Arrays.sort(nums);
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int j = i+1, k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < target) {
+                    counter += k - j;
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return counter;
     }
 }
