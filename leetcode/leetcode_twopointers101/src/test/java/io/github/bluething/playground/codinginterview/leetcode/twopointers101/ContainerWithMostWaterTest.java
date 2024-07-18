3925303780a97230d392a2f36f86557a6760c28f
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-18 12:19:34 +0700

    solution

--- leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/ContainerWithMostWaterTest.java
+++ leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/ContainerWithMostWaterTest.java
@@ -9,6 +9,17 @@ class ContainerWithMostWaterTest {
         Assertions.assertEquals(49, maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
     }
     private int maxArea(int[] height) {
-        return 0;
+        int left = 0, right = height.length - 1;
+        int maxArea = Integer.MIN_VALUE;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                maxArea = Math.max(maxArea, height[left] * (right - left));
+                left++;
+            } else {
+                maxArea = Math.max(maxArea, height[right] * (right - left));
+                right--;
+            }
+        }
+        return maxArea;
     }
 }
