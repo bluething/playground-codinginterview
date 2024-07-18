a2df258be1bf2e0da3f35efab7b6035a6217c97f
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-18 22:53:24 +0700

    Solution

--- leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/PartitionLabelsTest.java
+++ leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/PartitionLabelsTest.java
@@ -3,6 +3,7 @@ package io.github.bluething.playground.codinginterview.leetcode.twopointers101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class PartitionLabelsTest {
@@ -15,6 +16,23 @@ class PartitionLabelsTest {
         Assertions.assertEquals(List.of(10), partitionLabels("eccbbbbdec"));
     }
     private List<Integer> partitionLabels(String s) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        int[] lastIndex = new int[128];
+        for (int i = 0; i < chars.length; i++) {
+            lastIndex[chars[i]] = i;
+        }
+        int left = 0, right = 0, currIdx = 0;
+        while (currIdx < chars.length) {
+            right = Math.max(right, lastIndex[chars[currIdx]]);
+            if (right == currIdx) {
+                int size = right - left + 1;
+                result.add(size);
+                right++;
+                left = right;
+            }
+            currIdx++;
+        }
+        return result;
     }
 }
