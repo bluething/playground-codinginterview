6c76e56c3d709f8a77cf4c495c9e8d9b83e283e8
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-18 13:54:37 +0700

    solution

--- leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/_3sumTest.java
+++ leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/_3sumTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode.twopointers101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,6 +22,33 @@ class _3sumTest {
         Assertions.assertEquals(List.of(List.of(0,0,0)), threeSum(new int[]{0,0,0}));
     }
     private List<List<Integer>> threeSum(int[] nums) {
-        return null;
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int j = i+1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    res.add(List.of(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j-1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k+1]) {
+                        k--;
+                    }
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return res;
     }
 }
