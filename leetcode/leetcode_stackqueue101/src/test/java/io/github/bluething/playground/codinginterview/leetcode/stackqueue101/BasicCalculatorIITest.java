61fa674a18b9c9c4ec7fc7e3342a5a3306706f69
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-21 01:24:11 +0700

    solution

--- leetcode/leetcode_stackqueue101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/stackqueue101/BasicCalculatorIITest.java
+++ leetcode/leetcode_stackqueue101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/stackqueue101/BasicCalculatorIITest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode.stackqueue101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 class BasicCalculatorIITest {
     @Test
     void case01() {
@@ -17,6 +19,36 @@ class BasicCalculatorIITest {
         Assertions.assertEquals(5, calculate(" 3+5 / 2 "));
     }
     private int calculate(String s) {
-        return 0;
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        int number = 0;
+        char operator = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                number = number * 10 + c - '0';
+            }
+            if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
+                if (operator == '+') {
+                    stack.push(number);
+                } else if (operator == '-') {
+                    stack.push(-number);
+                } else if (operator == '*') {
+                    stack.push(stack.pop() * number);
+                } else if (operator == '/') {
+                    stack.push(stack.pop() / number);
+                }
+
+                operator = c;
+                number = 0;
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
     }
 }
