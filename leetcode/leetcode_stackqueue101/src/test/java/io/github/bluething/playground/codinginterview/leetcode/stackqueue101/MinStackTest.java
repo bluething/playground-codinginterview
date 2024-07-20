a7a5154691ab5571dd635b83f0c477b2b0e9cf92
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-20 11:34:08 +0700

    solution

--- leetcode/leetcode_stackqueue101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/stackqueue101/MinStackTest.java
+++ leetcode/leetcode_stackqueue101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/stackqueue101/MinStackTest.java
@@ -3,24 +3,42 @@ package io.github.bluething.playground.codinginterview.leetcode.stackqueue101;
 class MinStackTest {
     class MinStack {
 
+        private Node top;
+
         public MinStack() {
 
         }
 
         public void push(int val) {
-
+            if (top == null) {
+                top = new Node(val, val, null);
+            } else {
+                top = new Node(val, Math.min(top.min, val), top);
+            }
         }
 
         public void pop() {
-
+            top = top.next;
         }
 
         public int top() {
-            return 0;
+            return top.val;
         }
 
         public int getMin() {
-            return 0;
+            return top.min;
+        }
+
+        class Node {
+            private final int val;
+            private int min;
+            Node next;
+
+            Node(int val, int min, Node next) {
+                this.val = val;
+                this.min = min;
+                this.next = next;
+            }
         }
     }
 }
