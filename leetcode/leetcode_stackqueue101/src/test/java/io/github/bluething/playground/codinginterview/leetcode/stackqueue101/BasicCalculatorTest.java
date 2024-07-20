546c1b5b2beba97b848166b7fd53b1265f3a1da0
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-21 00:53:47 +0700

    solution

--- leetcode/leetcode_stackqueue101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/stackqueue101/BasicCalculatorTest.java
+++ leetcode/leetcode_stackqueue101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/stackqueue101/BasicCalculatorTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode.stackqueue101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 class BasicCalculatorTest {
     @Test
     void case01() {
@@ -18,6 +20,39 @@ class BasicCalculatorTest {
         Assertions.assertEquals(23, calculate("(1+(4+5+2)-3)+(6+8)"));
     }
     private int calculate(String s) {
-        return 0;
+        Stack<Integer> stack = new Stack<>();
+        int number = 0;
+        int sign = 1;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                number = number * 10 + (c - '0');
+            } else if (c == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                sign = 1;
+                result = 0;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
+        }
+
+        if (number != 0) {
+            result += sign * number;
+        }
+
+        return result;
     }
 }
