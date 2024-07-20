244ccab361db3056b3af2b20771109005dfce6ed
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-20 11:09:03 +0700

    solution

--- leetcode/leetcode_stackqueue101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/stackqueue101/ValidParenthesesTest.java
+++ leetcode/leetcode_stackqueue101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/stackqueue101/ValidParenthesesTest.java
@@ -30,6 +30,29 @@ class ValidParenthesesTest {
     }
 
     private boolean isValid(String s) {
-        return false;
+        char[] stack = new char[s.length()];
+        char[] pairs = new char[128];
+        pairs[')'] = '(';
+        pairs['}'] = '{';
+        pairs[']'] = '[';
+        int idx = 0;
+        for (char c : s.toCharArray()) {
+            // put open to the stack
+            if (pairs[c] == 0) {
+                stack[idx++] = c;
+            // meet close
+            } else {
+                // close doesn't have pair
+                if (idx == 0) {
+                    return false;
+                // top of the stack have wrong pair
+                } else if (stack[idx-1] != pairs[c]) {
+                    return false;
+                }
+                // pop the stack
+                idx--;
+            }
+        }
+        return idx == 0;
     }
 }
