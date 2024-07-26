ffddacd154d13aaac2aba85011f720a7d7cd0ccb
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-26 10:39:48 +0700

    solution

--- leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/LongestSubstringWithoutRepeatingCharactersTest.java
+++ leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode.slidingwindow101
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class LongestSubstringWithoutRepeatingCharactersTest {
     @Test
     void case01() {
@@ -35,6 +38,22 @@ class LongestSubstringWithoutRepeatingCharactersTest {
     }
 
     private int lengthOfLongestSubstring(String s) {
-        return 0;
+        int left = 0, right = 0;
+        Map<Character, Integer> uniqueCounter = new HashMap<>();
+        int maxLength = 0;
+        while (right < s.length()) {
+            uniqueCounter.put(s.charAt(right), uniqueCounter.getOrDefault(s.charAt(right), 0) + 1);
+            while (uniqueCounter.size() != (right-left+1)) {
+                uniqueCounter.put(s.charAt(left), uniqueCounter.get(s.charAt(left)) - 1);
+                if (uniqueCounter.get(s.charAt(left)) == 0) {
+                    uniqueCounter.remove(s.charAt(left));
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength, right-left+1);
+            right++;
+        }
+
+        return maxLength;
     }
 }
