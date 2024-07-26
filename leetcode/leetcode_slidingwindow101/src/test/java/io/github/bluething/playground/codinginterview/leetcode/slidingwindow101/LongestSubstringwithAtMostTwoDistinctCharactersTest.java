15d3713597a4b370c4f52a0af0a658a740c9f7b5
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-26 10:34:30 +0700

    solution

--- leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/LongestSubstringwithAtMostTwoDistinctCharactersTest.java
+++ leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/LongestSubstringwithAtMostTwoDistinctCharactersTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode.slidingwindow101
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringwithAtMostTwoDistinctCharactersTest {
     @Test
     void case01() {
@@ -15,6 +18,22 @@ public class LongestSubstringwithAtMostTwoDistinctCharactersTest {
     }
 
     private int lengthOfLongestSubstringTwoDistinct(String s) {
-        return 0;
+        int left = 0, right = 0;
+        Map<Character, Integer> uniqueCounter = new HashMap<>();
+        int maxLength = 0;
+        while (right < s.length()) {
+            uniqueCounter.put(s.charAt(right), uniqueCounter.getOrDefault(s.charAt(right), 0) + 1);
+            while (uniqueCounter.size() > 2) {
+                uniqueCounter.put(s.charAt(left), uniqueCounter.get(s.charAt(left)) - 1);
+                if (uniqueCounter.get(s.charAt(left)) == 0) {
+                    uniqueCounter.remove(s.charAt(left));
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength, right-left+1);
+            right++;
+        }
+
+        return maxLength;
     }
 }
