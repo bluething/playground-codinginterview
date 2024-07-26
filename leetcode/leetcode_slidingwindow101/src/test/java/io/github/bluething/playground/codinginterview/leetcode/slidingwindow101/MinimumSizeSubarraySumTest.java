f81f002815627f568be491452dcee8636b3ff0ad
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-26 10:16:35 +0700

    solution

--- leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/MinimumSizeSubarraySumTest.java
+++ leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/MinimumSizeSubarraySumTest.java
@@ -20,6 +20,18 @@ class MinimumSizeSubarraySumTest {
     }
 
     private int minSubArrayLen(int target, int[] nums) {
-        return 0;
+        int left = 0, right = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+        while (right < nums.length) {
+            sum += nums[right];
+            while (sum >= target) {
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left++];
+            }
+            right++;
+        }
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
     }
 }
