be713043f0e3e66641828d1d81bc25bb91877fc8
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-26 10:53:52 +0700

    solution

--- leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/MaxConsecutiveOnesIITest.java
+++ leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/MaxConsecutiveOnesIITest.java
@@ -13,6 +13,27 @@ class MaxConsecutiveOnesIITest {
         Assertions.assertEquals(4, findMaxConsecutiveOnes(new int[]{1,0,1,1,0,1}));
     }
     private int findMaxConsecutiveOnes(int[] nums) {
-        return 0;
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+
+        int left = 0, right = 0;
+        int maxLength = 0, counterOf0 = 0;
+        while (right < n) {
+            if (nums[right] == 0) {
+                counterOf0++;
+            }
+            while (counterOf0 > 1) {
+                if (nums[left] == 0) {
+                    counterOf0--;
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
+        }
+
+        return maxLength;
     }
 }
