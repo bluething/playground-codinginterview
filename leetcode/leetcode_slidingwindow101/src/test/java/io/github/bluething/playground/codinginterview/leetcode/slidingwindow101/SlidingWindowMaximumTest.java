c865f3ac5926e5011e4227696f72d5195df11a7d
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-26 22:30:46 +0700

    solution

--- leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/SlidingWindowMaximumTest.java
+++ leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/SlidingWindowMaximumTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode.slidingwindow101
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 class SlidingWindowMaximumTest {
     @Test
     void case01() {
@@ -10,9 +13,27 @@ class SlidingWindowMaximumTest {
     }
     @Test
     void case02() {
-        Assertions.assertArrayEquals(new int[]{1}, maxSlidingWindow(new int[]{1}, 3));
+        Assertions.assertArrayEquals(new int[]{1}, maxSlidingWindow(new int[]{1}, 1));
     }
     private int[] maxSlidingWindow(int[] nums, int k) {
-        return new int[]{};
+        int n = nums.length;
+        if (nums.length == 0 || k == 0) {
+            return new int[0];
+        }
+        int[] res = new int[n - k + 1];
+        Deque<Integer> window = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            while (!window.isEmpty() && window.peekFirst() <= i - k) {
+                window.pollFirst();
+            }
+            while (!window.isEmpty() && nums[window.peekLast()] < nums[i]) {
+                window.pollLast();
+            }
+            window.offerLast(i);
+            if (i >= k - 1) {
+                res[i - k + 1] = nums[window.peekFirst()];
+            }
+        }
+        return res;
     }
 }
