9c02e0558d46dc3010b8cf8ed66f7e8b77c868c2
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-26 16:13:49 +0700

    solution

--- leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/PermutationinStringTest.java
+++ leetcode/leetcode_slidingwindow101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/slidingwindow101/PermutationinStringTest.java
@@ -13,6 +13,32 @@ class PermutationinStringTest {
         Assertions.assertFalse(checkInclusion("ab", "eidboaoo"));
     }
     private boolean checkInclusion(String s1, String s2) {
-        return true;
+        int[] arr = new int[128];
+        int left = 0, right = 0;
+        char[] s1Chars = s1.toCharArray();
+        for (char c : s1Chars) {
+            arr[c]++;
+        }
+        char[] s2Chars = s2.toCharArray();
+        int minLength = Integer.MAX_VALUE;
+        int counter = 0;
+        while (right < s2.length()) {
+            char c = s2Chars[right];
+            if (--arr[c] >= 0) {
+                counter++;
+            }
+            while (counter == s1.length()) {
+                int currLength = right - left + 1;
+                minLength = Math.min(minLength, currLength);
+                char leftChar = s2Chars[left];
+                if (++arr[leftChar] > 0) {
+                    counter--;
+                }
+                left++;
+            }
+
+            right++;
+        }
+        return minLength == s1.length();
     }
 }
