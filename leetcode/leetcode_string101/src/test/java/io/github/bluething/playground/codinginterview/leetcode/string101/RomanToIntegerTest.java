eb72346f00cd3521721cb664cb724534bf17017c
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-30 10:08:37 +0700

    solution

--- leetcode/leetcode_string101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/string101/RomanToIntegerTest.java
+++ leetcode/leetcode_string101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/string101/RomanToIntegerTest.java
@@ -3,6 +3,9 @@ package io.github.bluething.playground.codinginterview.leetcode.string101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class RomanToIntegerTest {
     @Test
     void case01() {
@@ -20,6 +23,26 @@ class RomanToIntegerTest {
     }
 
     private int romanToInt(String s) {
-        return 0;
+        Map<Character, Integer> romans = new HashMap<>();
+        romans.put('I', 1);
+        romans.put('V', 5);
+        romans.put('X', 10);
+        romans.put('L', 50);
+        romans.put('C', 100);
+        romans.put('D', 500);
+        romans.put('M', 1000);
+
+        int result = 0;
+        int currentInt = 0, prevInt = 0;
+        for (char c : s.toCharArray()) {
+            currentInt = romans.get(c);
+            result += currentInt;
+            if (currentInt > prevInt) {
+                result -= prevInt * 2;
+            }
+            prevInt = currentInt;
+        }
+
+        return result;
     }
 }
