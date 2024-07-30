32a7223476b647bca20ad9a03066aa384b706074
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-30 11:56:47 +0700

    solution

--- leetcode/leetcode_string101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/string101/LongestCommonPrefixTest.java
+++ leetcode/leetcode_string101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/string101/LongestCommonPrefixTest.java
@@ -13,8 +13,46 @@ class LongestCommonPrefixTest {
     void case02() {
         Assertions.assertEquals("", longestCommonPrefix(new String[]{"dog","racecar","car"}));
     }
+    @Test
+    void case03() {
+        Assertions.assertEquals("fl", longestCommonPrefix2(new String[]{"flower","flow","flight"}));
+    }
+
+    @Test
+    void case04() {
+        Assertions.assertEquals("", longestCommonPrefix2(new String[]{"dog","racecar","car"}));
+    }
 
     private String longestCommonPrefix(String[] strs) {
-        return "";
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || c != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+    private String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
     }
 }
