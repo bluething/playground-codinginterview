78a3ada5d47d78b259328d3af0d3876b8fdec3f5
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-30 22:39:30 +0700

    solution

--- leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/ValidPalindromeTest.java
+++ leetcode/leetcode_twopointers101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/twopointers101/ValidPalindromeTest.java
@@ -30,6 +30,21 @@ class ValidPalindromeTest {
     }
 
     private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        s = s.toLowerCase();
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
         return true;
     }
 }
