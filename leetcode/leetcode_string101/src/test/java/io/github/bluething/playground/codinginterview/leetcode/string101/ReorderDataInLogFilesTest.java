56da578b8c6a17ea71167a7c4d241419b1b20b9e
Habib Ali Machpud <habib.machpud@gmail.com>
2024-07-30 13:50:11 +0700

    solution

--- leetcode/leetcode_string101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/string101/ReorderDataInLogFilesTest.java
+++ leetcode/leetcode_string101/src/test/java/io/github/bluething/playground/codinginterview/leetcode/string101/ReorderDataInLogFilesTest.java
@@ -3,6 +3,8 @@ package io.github.bluething.playground.codinginterview.leetcode.string101;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 class ReorderDataInLogFilesTest {
     @Test
     void case01() {
@@ -15,6 +17,28 @@ class ReorderDataInLogFilesTest {
                 reorderLogFiles(new String[]{"a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"}));
     }
     private String[] reorderLogFiles(String[] logs) {
-        return new String[]{};
+        Arrays.sort(logs, (s1, s2) -> {
+            String[] split1 = s1.split(" ", 2);
+            String[] split2 = s2.split(" ", 2);
+
+            boolean isS1Digit = Character.isDigit(split1[1].charAt(0));
+            boolean isS2Digit = Character.isDigit(split2[1].charAt(0));
+            
+            if (isS1Digit && isS2Digit) {
+                return 0;
+            } else if (isS1Digit) {
+                return 1;
+            } else if (isS2Digit) {
+                return -1;
+            } else {
+                int compare = split1[1].compareTo(split2[1]);
+                if (compare == 0) {
+                    return split1[0].compareTo(split2[0]);
+                } else {
+                    return compare;
+                }
+            }
+        });
+        return logs;
     }
 }
