b9cc88cb229468b8f8f6ab3eab3e1457bf0704ee
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-15 23:47:17 +0700

    new module

---
+++ codility/codility_lessons/src/main/java/io/github/bluething/playground/codinginterview/codility/Main.java
@@ -0,0 +1,7 @@
+package io.github.bluething.playground.codinginterview.codility;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
